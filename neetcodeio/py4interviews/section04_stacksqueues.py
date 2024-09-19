615a1d3465369b671d30cb4a67b8c4693c6a6f75
@iamserda <12580399+iamserda@users.noreply.github.com>
2024-09-19 18:15:53 -0400

    completed section 4, challenge 3.  -@iamserda

--- neetcodeio/py4interviews/section04_stacksqueues.py
+++ neetcodeio/py4interviews/section04_stacksqueues.py
@@ -46,3 +46,22 @@ def challenge2():
     # deque([1, 2, 3, 4, 5])
 
 
+# Challenge 3:
+def challenge3():
+    """pass"""
+
+    def rotate_list(arr: List[int], k: int) -> Deque[int]:
+        my_queue = deque(arr)
+        for _ in range(k):
+            my_queue.appendleft(my_queue.pop())
+        return my_queue
+
+    # do not modify below this line
+    print(rotate_list([1, 2, 3, 4, 5], 0))  # deque([1, 2, 3, 4, 5])
+    print(rotate_list([1, 2, 3, 4, 5], 1))  # deque([5, 1, 2, 3, 4])
+    print(rotate_list([1, 2, 3, 4, 5], 2))  # deque([4, 5, 1, 2, 3])
+    print(rotate_list([1, 2, 3, 4, 5], 3))  # deque([3, 4, 5, 1, 2])
+    print(rotate_list([1, 2, 3, 4, 5], 4))  # deque([2, 3, 4, 5, 1])
+    print(rotate_list([1, 2, 3, 4, 5], 5))  # deque([1, 2, 3, 4, 5])
+
+
