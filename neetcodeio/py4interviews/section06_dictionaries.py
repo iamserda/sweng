db0e74b1ddbc5b68d9d93aeb1e1593101a54f663
@iamserda <12580399+iamserda@users.noreply.github.com>
2024-09-21 11:30:53 -0400

    completed section 6, challenge 2, 2A -@iamserda

--- neetcodeio/py4interviews/section06_dictionaries.py
+++ neetcodeio/py4interviews/section06_dictionaries.py
@@ -1,5 +1,7 @@
 """pass """
 from typing import List, Dict
+from collections import defaultdict
+
 
 # challenge 1:
 def challenge1():
@@ -33,20 +35,118 @@ def challenge1():
 
 
 # challenge 2:
+def challenge2():
+    """pass """
+    def count_chars(s: str) -> Dict[str, int]:
+        my_dictio = {}
+        for char in s:
+            if char in my_dictio:
+                my_dictio[char] += 1
+            else:
+                my_dictio[char] = 1
+        return my_dictio
+
+
+    def nested_list_to_dict(nums: List[List[int]]) -> Dict[int, List[int]]:
+        results = {}
+        for row in nums:
+            results[row[0]] = []
+            for idx in range(1, len(row)):
+                results[row[0]].append(row[idx])
+        return results
+
+    # do not modify below this line
+    print(count_chars("hello"))
+    print(count_chars("helloworld"))
+    print(count_chars("areallylongstringwhyareyoureadingthishahalol"))
+    print(nested_list_to_dict([[1, 2, 3], [4, 5, 6], [1, 4]]))
+    print(nested_list_to_dict([[1, 2, 3, 4], [4, 5, 6, 7], [1, 4, 5, 6]]))
+    print(nested_list_to_dict([[5, 2, 3, 4, 5], [4, 5, 6, 7, 8], [5, 6, 7, 8, 9]]))
+    print(nested_list_to_dict([[3, 2, 3, 4, 5], [4, 5, 6, 7, 8], [5, 6, 7, 8]]))
+    # EXPECTED OUTPUT:
+    # {'h': 1, 'e': 1, 'l': 2, 'o': 1}
+    # {'h': 1, 'e': 1, 'l': 3, 'o': 2, 'w': 1, 'r': 1, 'd': 1}
+    # {'a': 6, 'r': 4, 'e': 3, 'l': 5, 'y': 3, 'o': 3, 'n': 3, 'g': 3, 's': 2, 't': 2, 'i': 3, 'w': 1, 'h': 4, 'u': 1, 'd': 1}
+    # {1: [4], 4: [5, 6]}
+    # {1: [4, 5, 6], 4: [5, 6, 7]}
+    # {5: [6, 7, 8, 9], 4: [5, 6, 7, 8]}
+    # {3: [2, 3, 4, 5], 4: [5, 6, 7, 8], 5: [6, 7, 8]}
+
+# challenge2a:
+def challenge2A():
+    """pass """
+    def count_chars(s: str) -> Dict[str, int]:
+        dictio = defaultdict(int)
+        for char in s:
+            dictio[char] += 1
+        return dictio
+
+    def nested_list_to_dict(nums: List[List[int]]) -> Dict[int, List[int]]:
+        results = defaultdict(list)
+        for row in nums:
+            results[row[0]] += row[1:]
+        return results
+
+    # do not modify below this line
+    print(count_chars("hello"))
+    print(count_chars("helloworld"))
+    print(count_chars("areallylongstringwhyareyoureadingthishahalol"))
+    print(nested_list_to_dict([[1, 2, 3], [4, 5, 6], [1, 4]]))
+    print(nested_list_to_dict([[1, 2, 3, 4], [4, 5, 6, 7], [1, 4, 5, 6]]))
+    print(nested_list_to_dict([[5, 2, 3, 4, 5], [4, 5, 6, 7, 8], [5, 6, 7, 8, 9]]))
+    print(nested_list_to_dict([[3, 2, 3, 4, 5], [4, 5, 6, 7, 8], [5, 6, 7, 8]]))
+
+    # EXPECTED OUTPUT:
+    # defaultdict(<class 'int'>, {'h': 1, 'e': 1, 'l': 2, 'o': 1})
+    # defaultdict(<class 'int'>, {'h': 1, 'e': 1, 'l': 3, 'o': 2, 'w': 1, 'r': 1, 'd': 1})
+    # defaultdict(<class 'int'>, {'a': 6, 'r': 4, 'e': 3, 'l': 5, 'y': 3, 'o': 3, 'n': 3, 'g': 3, 's': 2, 't': 2, 'i': 3, 'w': 1, 'h': 4, 'u': 1, 'd': 1})
+    # defaultdict(<class 'list'>, {1: [2, 3, 4], 4: [5, 6]})
+    # defaultdict(<class 'list'>, {1: [2, 3, 4, 4, 5, 6], 4: [5, 6, 7]})
+    # defaultdict(<class 'list'>, {5: [2, 3, 4, 5, 6, 7, 8, 9], 4: [5, 6, 7, 8]})
+    # defaultdict(<class 'list'>, {3: [2, 3, 4, 5], 4: [5, 6, 7, 8], 5: [6, 7, 8]})
+
+
+
 # challenge 3:
+def challenge3():
+    pass
+
 # challenge 4:
+def challenge4():
+    pass
+
 # challenge 5:
+def challenge5():
+    pass
+
 # challenge 6:
+def challenge6():
+    pass
+
 # challenge 7:
+def challenge7():
+    pass
+
 # challenge 8:
+def challenge8():
+    pass
 
 # TESTING GROUNDS:
-
+print("\nChallenge 1: ")
 challenge1()
-# challenge2()
-# challenge3()
-# challenge4()
-# challenge5()
-# challenge6()
-# challenge7()
-# challenge8()
+print("\nChallenge 2: ")
+challenge2()
+print("\nChallenge 2A: ")
+challenge2A()
+print("\nChallenge 3: ")
+challenge3()
+print("\nChallenge 4: ")
+challenge4()
+print("\nChallenge 5: ")
+challenge5()
+print("\nChallenge 6: ")
+challenge6()
+print("\nChallenge 7: ")
+challenge7()
+print("\nChallenge 8: ")
+challenge8()
