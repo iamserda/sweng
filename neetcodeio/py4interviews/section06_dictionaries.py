4381272bc1f35b3ec2958c80cbb2884b0facf89a
@iamserda <12580399+iamserda@users.noreply.github.com>
2024-09-22 01:03:48 -0400

    Completed Section 6 on Hashmaps(dict) and Hash Sets(set). Completed section 6, Challenge 8.  -@iamserda

--- neetcodeio/py4interviews/section06_dictionaries.py
+++ neetcodeio/py4interviews/section06_dictionaries.py
@@ -281,8 +281,40 @@ def challenge7():
 # challenge 8:
 def challenge8():
     """pass """
-    pass
+    def grid_to_set(grid: List[List[int]]) -> Set[Tuple[int, int]]:
+        my_set = set()
+        for row_index, row in enumerate(grid):
+            for col_index, col in enumerate(row):
+                if col == 1:
+                    my_set.add((row_index, col_index))
+        return my_set
 
+    # do not modify below this line
+    output1 = grid_to_set([[1, 0, 1], [0, 1, 0], [1, 0, 1]])
+    print(type(output1))
+    print(sorted(list(output1)))
+
+    output2 = grid_to_set([[1, 0, 0], [0, 0, 0]])
+    print(type(output2))
+    print(sorted(list(output2)))
+
+    output3 = grid_to_set([[1, 1, 1], [1, 1, 1]])
+    print(type(output3))
+    print(sorted(list(output3)))
+
+    output4 = grid_to_set([[0, 0, 0], [0, 0, 0], [0, 0, 0]])
+    print(type(output4))
+    print(sorted(list(output4)))
+
+    #Expected Ouput:
+    # <class 'set'>
+    # [(0, 0), (0, 2), (1, 1), (2, 0), (2, 2)]
+    # <class 'set'>
+    # [(0, 0)]
+    # <class 'set'>
+    # [(0, 0), (0, 1), (0, 2), (1, 0), (1, 1), (1, 2)]
+    # <class 'set'>
+    # []
 
 # TESTING GROUNDS:
 print("\nChallenge 1: ")
