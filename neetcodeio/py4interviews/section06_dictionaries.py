4bcf1372fb0826e192bdf4c90ce67c623fa0d13b
@iamserda <12580399+iamserda@users.noreply.github.com>
2024-09-22 01:02:07 -0400

    Completed section 6, Challenge 7.  -@iamserda

--- neetcodeio/py4interviews/section06_dictionaries.py
+++ neetcodeio/py4interviews/section06_dictionaries.py
@@ -248,11 +248,39 @@ def challenge6():
 
 # challenge 7:
 def challenge7():
-    pass
+    from typing import List, Set
+
+
+    def double_nums(nums: List[int]) -> Set[int]:
+        return {num * 2 for num in nums}
+
+
+    # do not modify below this line
+
+    output1 = double_nums([1, 2, 3])
+    print(type(output1)) 
+    print(sorted(list(output1)))
+
+    output2 = double_nums([4, -2, 0, 7])
+    print(type(output2)) 
+    print(sorted(list(output2)))
+
+    output3 = double_nums([10, 20, 30, 40, 50])
+    print(type(output3)) 
+    print(sorted(list(output3)))
+
+    # Expected Output:
+    # <class 'set'>
+    # [2, 4, 6]
+    # <class 'set'>
+    # [-4, 0, 8, 14]
+    # <class 'set'>
+    # [20, 40, 60, 80, 100]
 
 
 # challenge 8:
 def challenge8():
+    """pass """
     pass
 
 
