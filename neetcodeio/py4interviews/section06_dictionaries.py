ed1eab24b75983bec068c06906d4142a8bce3a47
@iamserda <12580399+iamserda@users.noreply.github.com>
2024-09-22 00:42:14 -0400

    Completed section 6, Challenge 6.  -@iamserda

--- neetcodeio/py4interviews/section06_dictionaries.py
+++ neetcodeio/py4interviews/section06_dictionaries.py
@@ -1,6 +1,6 @@
 """pass """
 
-from typing import List, Dict, Tuple, Counter as CounterType
+from typing import List, Dict, Tuple,Set, Counter as CounterType
 from collections import defaultdict
 
 
@@ -212,7 +212,38 @@ def challenge5A():
 
 # challenge 6:
 def challenge6():
-    pass
+    """pass """
+    def build_hash_set(keys: List[str]) -> Set[str]:
+        return set(keys)
+
+
+    def check_keys(hash_set: Set[str], keys: List[str]) -> List[bool]:
+        return [True if key in hash_set else False for key in keys]
+
+
+    # do not modify below this line
+
+    output1 = build_hash_set(["Alice", "Bob", "Charlie"])
+    print(type(output1))         # check the type of the output
+    print(sorted(list(output1))) # set order is not guaranteed so we need to sort the list
+
+    output2 = build_hash_set(["XY", "XX", "YY", "XY", "YX"]) 
+    print(type(output2))         # check the type of the output
+    print(sorted(list(output2))) # set order is not guaranteed so we need to sort the list
+
+    print(check_keys({"Alice", "Bob", "Charlie"}, ["Alice", "Bob", "Charlie", "David"]))
+    print(check_keys({'a', 'b', 'c'}, ['a', 'd', 'c']))
+    print(check_keys({'a', 'c'}, ['d', 'c']))
+
+    # Expected Output:
+    # <class 'set'>
+    # ['Alice', 'Bob', 'Charlie']
+    # <class 'set'>
+    # ['XX', 'XY', 'YX', 'YY']
+    # [True, True, True, False]
+    # [True, False, True]
+    # [False, True]
+
 
 
 # challenge 7:
