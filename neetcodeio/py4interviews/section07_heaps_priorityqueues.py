916a6f5cef71fc0c487bdca1d96fd0d7ede73976
@iamserda <12580399+iamserda@users.noreply.github.com>
2024-09-22 21:51:14 -0400

    Starting Section 07 on Heaps(max_heaps, min_heaps) and Priority Queues. Completed section 7, Challenge 2.  -@iamserda

--- neetcodeio/py4interviews/section07_heaps_priorityqueues.py
+++ neetcodeio/py4interviews/section07_heaps_priorityqueues.py
@@ -17,7 +17,16 @@ def challenge1():
 
 def challenge2():
     """pass"""
-    pass
+    def heap_pop(heap: List[int]):
+        return [heapq.heappop(heap) for i in range(len(heap))]
+    # do not modify below this line
+    print(heap_pop([1, 2, 3]))
+    print(heap_pop([1, 3, 2]))
+    print(heap_pop([6, 7, 8, 12, 9, 10]))
+    # EXPECTED OUTPUT:
+    # [1, 2, 3]
+    # [1, 2, 3]
+    # [6, 7, 8, 9, 10, 12]
 
 def challenge3():
     """pass"""
