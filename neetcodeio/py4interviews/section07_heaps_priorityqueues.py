8c389129716aa45ad18975dd83d40a12641f3150
@iamserda <12580399+iamserda@users.noreply.github.com>
2024-09-22 22:09:55 -0400

    Starting Section 07 on Heaps(max_heaps, min_heaps) and Priority Queues. Completed section 7, Challenge 3.  -@iamserda

--- neetcodeio/py4interviews/section07_heaps_priorityqueues.py
+++ neetcodeio/py4interviews/section07_heaps_priorityqueues.py
@@ -30,7 +30,29 @@ def challenge2():
 
 def challenge3():
     """pass"""
-    pass
+    def heapify_strings(strings: List[str]) -> List[str]:
+        heapq.heapify(strings)
+        return strings
+
+
+    def heapify_integers(integers: List[int]) -> List[int]:
+        heapq.heapify(integers)
+        return integers
+
+    def heap_sort(nums: List[int]) -> List[int]:
+        heapq.heapify(nums)
+        return sorted(nums)
+        
+
+    # do not modify below this line
+    print(heapify_strings(["b", "a", "e", "c", "d"]))
+    print(heapify_integers([3, 4, 5, 1, 2, 6]))
+    print(heap_sort([3, 4, 5, 1, 2, 6]))
+    # EXPECTED OUTPUT:
+    # ['a', 'b', 'e', 'c', 'd']
+    # [1, 2, 5, 4, 3, 6]
+    # [1, 2, 3, 4, 5, 6]
+
 
 def challenge4():
     """pass"""
