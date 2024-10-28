9a3bdda7503cae7068639453f27f98b88a4a6437
@iamserda <12580399+iamserda@users.noreply.github.com>
2024-10-27 22:24:14 -0400

    solved: neetcode: contains_duplicate solved by first sorting, then comparing values a given index to the value before it. -@iamserda

--- neetcodeio/roadmap/arrays_and_hashing/1ContainsDuplicates.py
+++ neetcodeio/roadmap/arrays_and_hashing/1ContainsDuplicates.py
@@ -7,7 +7,17 @@ class Solution:
                 continue
         return False
 
+    def has_duplicate_sorting(self, nums: list[int]) -> bool:
+        nums.sort() # sort in-place
+        for idx in range(1, len(nums)):
+            if nums[idx - 1] == nums[idx]:
+                return True
+        return False
+
 #Testing Arenas:
 sol = Solution()
 assert sol.hasDuplicateNaive([1,2,3,3]) is True
 assert sol.hasDuplicateNaive([1,2,3,4]) is False
+
+assert sol.has_duplicate_sorting([1,2,3,3]) is True
+assert sol.has_duplicate_sorting([1,2,3,4]) is False
