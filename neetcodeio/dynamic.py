273ed329cad223054b9155ef6e6da1ad2475c488
@iamserda <12580399+iamserda@users.noreply.github.com>
2025-01-11 22:36:05 -0500

    solved: improved set and get methods, added better error handling. -@iamserda

--- neetcodeio/dynamic.py
+++ neetcodeio/dynamic.py
@@ -3,24 +3,15 @@ class DynamicArray:
     def __init__(self, capacity: int):
         self.array: list = [None]*capacity
         self.capacity = capacity
-        
+
     def get(self, i: int) -> int:
-        if self.capacity == 0:
-            raise IndexError("Cannot uset get() on an empty Dynamic Array instance.")
+        if len(self.array) == 0:
+            raise IndexError("Cannot use method 'get()' on an empty Dynamic Array instance.")
         if i >= self.capacity:
             raise IndexError("Requested index is out of range.")
         return self.array[i]
     
     def set(self, i: int, n: int) -> None:
+        if i >= self.capacity:
+            raise IndexError("Index is out of range for this task. Cannot set a new value at this index. Consider .pushback() method.")
         self.array[i] = n
-
-    def pushback(self, n: int) -> None:
-    # def popback(self) -> int:
-    # def resize(self) -> None:
-    # def getSize(self) -> int:
-
-    def getCapacity(self) -> int:
-        return self.capacity
-
-dyn_arr = DynamicArray(3)
-assert dyn_arr.getCapacity() == 3
