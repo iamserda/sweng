0104d53a7ade59e917e17a997dbbf4e5385862b0
@iamserda <12580399+iamserda@users.noreply.github.com>
2025-01-11 22:09:22 -0500

    solved: .set() -@iamserda

--- neetcodeio/dynamic.py
+++ neetcodeio/dynamic.py
@@ -1,26 +1,26 @@
 class DynamicArray:
     
     def __init__(self, capacity: int):
-        self.array: list = []
-        self.capacity: int = capacity
+        self.array: list = [None]*capacity
+        self.capacity = capacity
         
     def get(self, i: int) -> int:
         if self.capacity == 0:
             raise IndexError("Cannot uset get() on an empty Dynamic Array instance.")
-        if i > self.capacity:
+        if i >= self.capacity:
             raise IndexError("Requested index is out of range.")
         return self.array[i]
     
     def set(self, i: int, n: int) -> None:
+        self.array[i] = n
 
     def pushback(self, n: int) -> None:
-
-    def popback(self) -> int:
- 
-
-    def resize(self) -> None:
-
-
-    def getSize(self) -> int:
+    # def popback(self) -> int:
+    # def resize(self) -> None:
+    # def getSize(self) -> int:
 
     def getCapacity(self) -> int:
+        return self.capacity
+
+dyn_arr = DynamicArray(3)
+assert dyn_arr.getCapacity() == 3
