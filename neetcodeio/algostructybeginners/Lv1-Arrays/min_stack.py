8ed5a764cb0c17b8a11562c28109814b50f5c431
@iamserda <12580399+iamserda@users.noreply.github.com>
2025-01-26 01:16:43 -0500

    solved: LeetCode #155 Min Stack on neetcode -@iamserda

--- neetcodeio/algostructybeginners/Lv1-Arrays/min_stack.py
+++ neetcodeio/algostructybeginners/Lv1-Arrays/min_stack.py
@@ -2,27 +2,30 @@ class MinStack:
 
     def __init__(self):
         self.stack = []
-        self.min_stack = []
-    
+        self.minstack = []
+
     def push(self, val: int) -> None:
-        self.stack.append(val)
-        if self.min_stack and val < self.min_stack[-1]:
-            popped = self.min_stack.pop()
-            self.min_stack.append(val)
-            self.min_stack.append(popped)
-        else:
-            self.min_stack.append(val)
-    
+        if val is not None:
+            self.stack.append(val)
+            if not self.minstack:
+                self.minstack.append(val)
+            elif val < self.minstack[-1]:
+                self.minstack.append(val)
+            else:
+                self.minstack.append(self.minstack[-1])
+
     def pop(self) -> None:
         if self.stack:
-            popped = self.stack.pop()
-        self.min_stack.pop() if popped == self.min_stack[-1] else None
-    
+            self.stack.pop()
+            self.minstack.pop()
+
     def top(self) -> int:
-        
+        if self.stack:
+            return self.stack[-1]
 
     def getMin(self) -> int:
-        
+        if self.stack:
+            return self.minstack[-1]
 
 
 # Your MinStack object will be instantiated and called as such:
