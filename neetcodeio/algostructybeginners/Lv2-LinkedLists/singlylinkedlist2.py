d9a99ebc1dccc4c57762cf46877e207c6584daad
@iamserda <12580399+iamserda@users.noreply.github.com>
2025-01-29 23:44:53 -0500

    redo practice singly-linked-list -@iamserda

---
+++ neetcodeio/algostructybeginners/Lv2-LinkedLists/singlylinkedlist2.py
@@ -0,0 +1,23 @@
+"""Challenge: Design a Singly-Linked Lists with the following methods: get, insertHead, insertTail, remove and getValues.
+"""
+
+
+class LinkedList:
+
+    def __init__(self):
+        pass
+
+    def get(self, index: int) -> int:
+        pass
+
+    def insertHead(self, val: int) -> None:
+        pass
+
+    def insertTail(self, val: int) -> None:
+        pass
+
+    def remove(self, index: int) -> bool:
+        pass
+
+    def getValues(self) -> list[int]:
+        pass
