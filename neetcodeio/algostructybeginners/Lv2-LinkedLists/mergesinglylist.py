587bcfb09a85ee59f9c8dc7be8a5608c73ca295d
@iamserda <12580399+iamserda@users.noreply.github.com>
2025-01-30 23:39:19 -0500

    improved previous solution, shorter. Found a solution that has the same time and space complexity but it is shorter. I think I might want to practice this one to save time.  -@iamserda

--- neetcodeio/algostructybeginners/Lv2-LinkedLists/mergesinglylist.py
+++ neetcodeio/algostructybeginners/Lv2-LinkedLists/mergesinglylist.py
@@ -1,4 +1,9 @@
-# Definition for singly-linked list.
+"""
+Problem: 
+Given the head pointer to two sorted linked list, merge the two sorted linked-list,
+to result in a sorted linked. return the head of the newly sorted linked-list.
+"""
+
 class ListNode:
     def __init__(self, val=0, next=None):
         self.val = val
@@ -6,50 +11,29 @@ class ListNode:
 
 
 class Solution:
-    def mergeTwoLists(self, list1: ListNode, list2: ListNode) -> ListNode:
-        if not list1 and not list2:
-            return None
-        if not list1:
-            return list2
-        if not list2:
-            return list1
 
+    def mergeTwoLists(self, list1: ListNode, list2: ListNode) -> ListNode:
         pre = ListNode(-1)
         current = pre
         temp1 = list1
         temp2 = list2
-        while temp1 or temp2:
-            if temp1:
-                if temp2:
-                    if temp1.val <= temp2.val:
-                        node = temp1
-                        temp1 = temp1.next
-                        node.next = None
-                        current.next = node
-                        current = current.next
-                        continue
-                    node = temp2
-                    temp2 = temp2.next
-                    node.next = None
-                    current.next = node
-                    current = current.next
-                    continue
-                node = temp1
+        while temp1 and temp2:
+            if temp1.val <= temp2.val:
+                current.next = temp1
                 temp1 = temp1.next
-                node.next = None
-                current.next = node
                 current = current.next
-                continue
-            if temp2:
-                node = temp2
+                current.next = None
+            else:
+                current.next = temp2
                 temp2 = temp2.next
-                node.next = None
-                current.next = node
                 current = current.next
-                continue
+                current.next = None
+        if temp1 or temp2:
+            current.next = temp1 if temp1 else temp2
         return pre.next if pre.next else None
 
 
+# TESTING ARENA:
 s1 = ListNode(11)
 s1.next = ListNode(32)
 s1.next.next = ListNode(53)
