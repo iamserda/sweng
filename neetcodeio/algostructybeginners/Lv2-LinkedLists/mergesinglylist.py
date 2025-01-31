5f813ef6f136fe66b3a10497aad20261a4636a38
@iamserda <12580399+iamserda@users.noreply.github.com>
2025-01-30 23:16:57 -0500

    solved: leetcode #21, neetcode merge-two-sorted-list -@iamserda

--- neetcodeio/algostructybeginners/Lv2-LinkedLists/mergesinglylist.py
+++ neetcodeio/algostructybeginners/Lv2-LinkedLists/mergesinglylist.py
@@ -1,9 +1,13 @@
+<<<<<<< HEAD
 """
 Problem: 
 Given the head pointer to two sorted linked list, merge the two sorted linked-list,
 to result in a sorted linked. return the head of the newly sorted linked-list.
 """
 
+=======
+# Definition for singly-linked list.
+>>>>>>> 6c8d8ed (solved: leetcode #21, neetcode merge-two-sorted-list -@iamserda)
 class ListNode:
     def __init__(self, val=0, next=None):
         self.val = val
@@ -11,12 +15,24 @@ class ListNode:
 
 
 class Solution:
+<<<<<<< HEAD
 
     def mergeTwoLists(self, list1: ListNode, list2: ListNode) -> ListNode:
+=======
+    def mergeTwoLists(self, list1: ListNode, list2: ListNode) -> ListNode:
+        if not list1 and not list2:
+            return None
+        if not list1:
+            return list2
+        if not list2:
+            return list1
+
+>>>>>>> 6c8d8ed (solved: leetcode #21, neetcode merge-two-sorted-list -@iamserda)
         pre = ListNode(-1)
         current = pre
         temp1 = list1
         temp2 = list2
+<<<<<<< HEAD
         while temp1 and temp2:
             if temp1.val <= temp2.val:
                 current.next = temp1
@@ -34,6 +50,40 @@ class Solution:
 
 
 # TESTING ARENA:
+=======
+        while temp1 or temp2:
+            if temp1:
+                if temp2:
+                    if temp1.val <= temp2.val:
+                        node = temp1
+                        temp1 = temp1.next
+                        node.next = None
+                        current.next = node
+                        current = current.next
+                        continue
+                    node = temp2
+                    temp2 = temp2.next
+                    node.next = None
+                    current.next = node
+                    current = current.next
+                    continue
+                node = temp1
+                temp1 = temp1.next
+                node.next = None
+                current.next = node
+                current = current.next
+                continue
+            if temp2:
+                node = temp2
+                temp2 = temp2.next
+                node.next = None
+                current.next = node
+                current = current.next
+                continue
+        return pre.next if pre.next else None
+
+
+>>>>>>> 6c8d8ed (solved: leetcode #21, neetcode merge-two-sorted-list -@iamserda)
 s1 = ListNode(11)
 s1.next = ListNode(32)
 s1.next.next = ListNode(53)
