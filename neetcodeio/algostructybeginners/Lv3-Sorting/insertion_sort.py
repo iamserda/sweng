40391340353e4bd63da15d7f5483842b385bb93a
@iamserda <12580399+iamserda@users.noreply.github.com>
2025-02-11 21:21:08 -0500

    minor: improved on naming convention and lookups are forward-looking instead of comparing to the previous val, we compare to the next val. -@iamserda

--- neetcodeio/algostructybeginners/Lv3-Sorting/insertion_sort.py
+++ neetcodeio/algostructybeginners/Lv3-Sorting/insertion_sort.py
@@ -1,22 +1,23 @@
-# Definition for a pair.
-# class Pair:
-#     def __init__(self, key: int, value: str):
-#         self.key = key
-#         self.value = value
+import random
+
 class Solution:
-    def insertionSort(self, pairs: list) -> list:
-        for index in range(len(pairs)):
-            jndex = index
-            while jndex > 0 and pairs[jndex] < pairs[jndex - 1]:
-                pairs[jndex], pairs[jndex - 1] = pairs[jndex - 1], pairs[jndex]
+
+    def insertionSort(self, arr: list) -> list:
+        for index in range(1, len(arr)):
+            jndex = index - 1
+            while jndex >= 0 and arr[jndex + 1] < arr[jndex]:
+                arr[jndex + 1], arr[jndex] = arr[jndex], arr[jndex + 1]
                 jndex -= 1
-        return pairs
+        return arr
 
 
-sol1 = Solution()
-arr = [5, 4, 3, 2, 1, 0]
-assert sol1.insertionSort(arr) == [0, 1, 2, 3, 4, 5]
+sol = Solution()
+
+arr = [10, 1, 20, 2, 30, 3, 4, 40, 5]
+assert sol.insertionSort(arr) == [1, 2, 3, 4, 5, 10, 20, 30, 40]
+
 arr = [0, 4, 3, 2, 4, 0]
-assert sol1.insertionSort(arr) == [0, 0, 2, 3, 4, 4]
-arr = [0, 1, 3, 5, 4, 2]
-assert sol1.insertionSort(arr) == [0, 1, 2, 3, 4, 5]
+assert sol.insertionSort(arr) == [0, 0, 2, 3, 4, 4]
+
+arr = [1, 3, 5, 4, 2]
+assert sol.insertionSort(arr) == [1, 2, 3, 4, 5]
