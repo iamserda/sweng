ae06f9c8597066eb3b2eca2291bcd97496233e42
@iamserda <12580399+iamserda@users.noreply.github.com>
2025-02-14 00:02:53 -0500

    updated: fixed edge cases. -@iamserda

--- neetcodeio/merge_sorted_arrays.py
+++ neetcodeio/merge_sorted_arrays.py
@@ -1,17 +1,31 @@
 def merge_sorted_arrays(arr1, arr2):
+    if not arr1 and not arr2:
+        return []
+    if not arr1:
+        return arr2
+    if not arr2:
+        return arr1
+
     arr3 = []
-    ptr1 = 0
-    ptr2 = 0
-    while ptr1 < len(arr1) or ptr2 < len(arr2):
-        if ptr1 >= len(arr1) or arr1[ptr1] >= arr2[ptr2]:
-            arr3.append(arr2[ptr2])
-            ptr2 += 1
+    index1 = 0
+    index2 = 0
+    while index1 < len(arr1) or index2 < len(arr2):
+        if index1 >= len(arr1):
+            arr3.append(arr2[index2])
+            index2 += 1
             continue
-        if ptr2 >= len(arr2) or arr1[ptr1] <= arr2[ptr2]:
-            arr3.append(arr1[ptr1])
-            ptr1 += 1
+        if index2 >= len(arr2):
+            arr3.append(arr1[index1])
+            index1 += 1
             continue
-        break
+        if arr1[index1] <= arr2[index2]:
+            arr3.append(arr1[index1])
+            index1 += 1
+            continue
+        if arr1[index1] >= arr2[index2]:
+            arr3.append(arr2[index2])
+            index2 += 1
+            continue
 
     return arr3
 
@@ -19,5 +33,11 @@ def merge_sorted_arrays(arr1, arr2):
 arr1 = [1, 3, 5, 7]
 arr2 = [0, 2, 4, 5, 7, 8]
 arr3 = merge_sorted_arrays(arr1, arr2)
-print(arr3)
-assert arr3 == sorted([*arr1, *arr2])
+# print(arr3)
+# assert arr3 == sorted([*arr1, *arr2])
+
+arr1 = [1]
+arr2 = [0]
+arr3 = merge_sorted_arrays(arr1, arr2)
+# print(arr3)
+# assert arr3 == sorted([*arr1, *arr2])
