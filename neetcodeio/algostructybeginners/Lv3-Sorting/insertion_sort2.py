9ae97503b71bf78329d155d798647eeb5e74ad19
@iamserda <12580399+iamserda@users.noreply.github.com>
2025-02-15 01:23:44 -0500

    solved: insertion_sort, try #2 -@iamserda

--- neetcodeio/algostructybeginners/Lv3-Sorting/insertion_sort2.py
+++ neetcodeio/algostructybeginners/Lv3-Sorting/insertion_sort2.py
@@ -9,15 +9,24 @@ class Solution:
         for i in range(1, len(arr)):
             j = i - 1
             while j >= 0 and arr[j] > arr[j+1]:
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]
+                j -= 1
+        return arr
 
-                
+
 sol = Solution()
 
 arr = [10, 1, 20, 2, 30, 3, 4, 40, 5]
-assert sol.insertionSort(arr) == [1, 2, 3, 4, 5, 10, 20, 30, 40]
+arr_ = sol.insertionSort(arr)
+print(arr_)
+assert arr_ == [1, 2, 3, 4, 5, 10, 20, 30, 40]
 
 arr = [0, 4, 3, 2, 4, 0]
+arr_ = sol.insertionSort(arr)
+print(arr_)
 assert sol.insertionSort(arr) == [0, 0, 2, 3, 4, 4]
 
 arr = [1, 3, 5, 4, 2]
+arr_ = sol.insertionSort(arr)
+print(arr_)
 assert sol.insertionSort(arr) == [1, 2, 3, 4, 5]
