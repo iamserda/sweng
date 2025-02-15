185537671fe0a8cedb0e4c318569cd9af9952b4d
@iamserda <12580399+iamserda@users.noreply.github.com>
2025-02-15 02:17:32 -0500

    setup: leetcode problem 1672, computing richest customer's wealth -@iamserda

---
+++ leetcode/1672/solutions.py
@@ -0,0 +1,9 @@
+class Solution:
+    def maximumWealth(self, accounts: list[list[int]]) -> int:
+        pass
+
+
+sol = Solution()
+assert sol.maximumWealth([[1, 2, 3], [3, 2, 1]]) == 6
+assert sol.maximumWealth([[1, 2, 3], [4, 5, 7], [1, 3, 9]]) == 16
+assert sol.maximumWealth([[2, 8, 7], [7, 1, 3], [1, 9, 5]]) == 17
