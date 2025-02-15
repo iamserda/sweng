abfee019227fb16f115a1abd0b453a33e4198e29
@iamserda <12580399+iamserda@users.noreply.github.com>
2025-02-14 23:59:48 -0500

    wip: attempting insertion sort 24h 48h later to make sure I understand it -@iamserda

--- neetcodeio/algostructybeginners/Lv3-Sorting/insertion_sort2.py
+++ neetcodeio/algostructybeginners/Lv3-Sorting/insertion_sort2.py
@@ -8,7 +8,7 @@ class Solution:
             return arr
         for i in range(1, len(arr)):
             j = i - 1
-            while j >= 0 and arr[j] > arr[j+1]:
+            while j >= 0 and arr[j] > arr[j + 1]:
                 arr[j], arr[j + 1] = arr[j + 1], arr[j]
                 j -= 1
         return arr
