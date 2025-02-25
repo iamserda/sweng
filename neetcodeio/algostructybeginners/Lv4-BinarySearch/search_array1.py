860b4688c00d90b92a331630d3c7d56acd4802af
@iamserda <12580399+iamserda@users.noreply.github.com>
2025-02-24 21:33:47 -0500

    solved: practicing binary_search 1X -@iamserda

--- neetcodeio/algostructybeginners/Lv4-BinarySearch/search_array1.py
+++ neetcodeio/algostructybeginners/Lv4-BinarySearch/search_array1.py
@@ -1,18 +1,22 @@
-import math
-
+import math, random
 
 class Solution:
     def binary_search(self, arr, val):
         left = 0
         right = len(arr) - 1
         while left <= right:
-            mid = int(math.ceil((right - left) // 2))
-            if arr[mid] == val:
-                return mid
+            mid = (right + left) // 2
             if val > arr[mid]:
-                
+                left = mid + 1
+            elif val < arr[mid]:
+                right = mid - 1
+            else:
+                return mid
+        return -1
 
-
-arr = [i for i in range(1, 101)]
+arr = [i for i in range(-100, 100)]
 sol = Solution()
-sol.binary_search(arr, val)
+index = sol.binary_search(arr, 10)
+assert sol.binary_search(arr, val=10) != -1  # Found
+index = sol.binary_search(arr, 400)
+assert sol.binary_search(arr, val=400) == -1  # Not Found
