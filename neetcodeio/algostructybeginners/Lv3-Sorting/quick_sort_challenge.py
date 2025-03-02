2b2635f7cefc8f19e30ecf9857ec9cbfdcfe6dea
@iamserda <12580399+iamserda@users.noreply.github.com>
2025-03-01 23:29:13 -0500

    setup: configuring quick_sort challenge -@iamserda

---
+++ neetcodeio/algostructybeginners/Lv3-Sorting/quick_sort_challenge.py
@@ -0,0 +1,8 @@
+# Definition for a pair.
+# class Pair:
+#     def __init__(self, key: int, value: str):
+#         self.key = key
+#         self.value = value
+class Solution:
+    def quickSort(self, pairs: List[Pair]) -> List[Pair]:
+        
