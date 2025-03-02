781423c791d18852f2ba2ea370ec2759abd541df
@iamserda <12580399+iamserda@users.noreply.github.com>
2025-03-02 17:56:14 -0500

    solved: quick_sort practice 1x. I don't fully understand the mecahanics yet but I am getting there.  -@iamserda

--- neetcodeio/algostructybeginners/Lv3-Sorting/quick_sort_solution1.py
+++ neetcodeio/algostructybeginners/Lv3-Sorting/quick_sort_solution1.py
@@ -1,23 +1,29 @@
-# Definition for a pair.
-class Pair:
-    def __init__(self, key: int, value: str):
-        self.key = key
-        self.value = value
+import random
 
 
 class Solution:
-    def quick_merge(arr1, arr2):
-        
-    def quickSort(self, pairs: list[Pair]) -> list[Pair]:
-        if len(pairs) == 1:
-            return pairs
-        else:
-            pivot = len(pairs) - 1
-            nxt = 0
-            for i in range(len(pairs) - 2):
-                if pairs[i] <= pairs[pivot]:
-                    pairs[i], pairs[nxt] = pairs[nxt], pairs[i]
-                    nxt += 1
-            pairs[pivot], pairs[nxt] = pairs[nxt], pairs[pivot]
-            first_half = self.quickSort(pairs[: nxt + 1])
-            second_half = self.quickSort(pairs[nxt + 1 :])
+
+    def quickSort(self, arr):
+        start, end = 0, len(arr)
+        return self.quick_sorter(arr, start, end)
+
+    def quick_sorter(self, arr, start, end):
+        if end - start + 1 <= 1:
+            return
+        pivot_index = end - 1
+        next_index = start
+        for index in range(start, pivot_index):
+            if arr[index] < arr[pivot_index]:
+                arr[index], arr[next_index] = arr[next_index], arr[index]
+                next_index += 1
+        arr[next_index], arr[pivot_index] = arr[pivot_index], arr[next_index]
+        self.quick_sorter(arr, start, next_index)
+        self.quick_sorter(arr, next_index + 1, end)
+        return arr
+
+
+sol = Solution()
+arr = [random.randrange(1, 100) for i in range(5)]
+sorted_arr = sorted(arr)
+sol.quickSort(arr)
+assert arr == sorted_arr
