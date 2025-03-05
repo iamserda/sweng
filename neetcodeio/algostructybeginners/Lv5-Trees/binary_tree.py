be04af1551148d98c92be2efdd7ca74e7191acc2
@iamserda <12580399+iamserda@users.noreply.github.com>
2025-03-04 21:46:58 -0500

    minor: added visual aid for structure of a binary tree. This is not a BST, it is just a binary tree. -@iamserda

--- neetcodeio/algostructybeginners/Lv5-Trees/binary_tree.py
+++ neetcodeio/algostructybeginners/Lv5-Trees/binary_tree.py
@@ -11,5 +11,17 @@ class BinaryTree:
 
 
 # TESTING ARENAS:
+#        8
+#     /    \
+#    10     1
+#   /  \    /\
+#  2   11  2  12
+
 my_binary_tree = BinaryTree()
 my_binary_tree.root = TreeNode(8)
+my_binary_tree.root.left = TreeNode(10)
+my_binary_tree.root.right = TreeNode(1)
+my_binary_tree.root.left.left = TreeNode(2)
+my_binary_tree.root.left.right = TreeNode(11)
+my_binary_tree.root.right.left = TreeNode(2)
+my_binary_tree.root.right.right = TreeNode(12)
