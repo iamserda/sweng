8a030910e0114b6fa9e2c18477b863d0d496283f
@iamserda <12580399+iamserda@users.noreply.github.com>
2025-03-05 03:15:18 -0500

    solved: traversing binary tree using BFS by queueing child nodes and printing the current node being visited, then dequeing that node. Very clever algorithm.  -@iamserda

--- neetcodeio/algostructybeginners/Lv5-Trees/binary_tree.py
+++ neetcodeio/algostructybeginners/Lv5-Trees/binary_tree.py
@@ -1,3 +1,5 @@
+from collections import deque
+
 class TreeNode:
     def __init__(self, value=None):
         self.value = value
@@ -9,6 +11,62 @@ class BinaryTree:
     def __init__(self):
         self.root = None
 
+    def recursive_insert(self, value):
+        def recurse(_temp, _new):
+            if _new.value > _temp.value:
+                if not _temp.right:
+                    _temp.right = _new
+                    return True
+                else:
+                    return recurse(_temp.right, _new)
+            elif _new.value < _temp.value:
+                if not _temp.left:
+                    _temp.left = _new
+                    return True
+                else:
+                    return recurse(_temp.left, _new)
+            return False
+
+        new_node = TreeNode(value)
+        if not self.root:
+            self.root = new_node
+            return True
+        else:
+            temp = self.root
+            return recurse(temp, new_node)
+
+    def insert(self, value) -> TreeNode:
+        new_node = TreeNode(value)
+        if not self.root:
+            self.root = new_node
+        else:
+            my_queue = deque()
+            my_queue.append(self.root)
+            while my_queue:
+                temp = my_queue.popleft()
+                if not temp.left:
+                    temp.left = new_node
+                    break
+                elif not temp.right:
+                    temp.right = new_node
+                    break
+                else:
+                    my_queue.append(temp.left)
+                    my_queue.append(temp.right)
+        return self.root
+
+    def print_binary_tree(self) -> None:
+        "BFS traversal"
+        arr = []
+
+        arr.append(self.root)
+        for index, elem in enumerate(arr):
+            if elem.left:
+                arr.append(elem.left)
+            if elem.right:
+                arr.append(elem.right)
+            print(f"node #{index + 1}: {elem.value}")
+
 
 # TESTING ARENAS:
 #        8
@@ -17,11 +75,8 @@ class BinaryTree:
 #   /  \    /\
 #  2   11  2  12
 
-my_binary_tree = BinaryTree()
-my_binary_tree.root = TreeNode(8)
-my_binary_tree.root.left = TreeNode(10)
-my_binary_tree.root.right = TreeNode(1)
-my_binary_tree.root.left.left = TreeNode(2)
-my_binary_tree.root.left.right = TreeNode(11)
-my_binary_tree.root.right.left = TreeNode(2)
-my_binary_tree.root.right.right = TreeNode(12)
+my_bin_tree = BinaryTree()
+for i in [8, 10, 1, 2, 11, 2, 12]:
+    my_bin_tree.insert(i)
+
+my_bin_tree.print_binary_tree()
