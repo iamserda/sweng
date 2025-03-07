59707fb222f8c3561087e9d225c7f87c5fad263f
@iamserda <12580399+iamserda@users.noreply.github.com>
2025-03-07 15:30:57 -0500

    solved: practicing bfs with a binary tree -@iamserda

---
+++ neetcodeio/algostructybeginners/Lv5-Trees/binary_tree_practice.py
@@ -0,0 +1,55 @@
+from collections import deque
+
+
+class TreeNode:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+
+
+class Tree:
+    def __init__(self):
+        self.root = None
+        self.height = 0
+
+    def iter_search(self, value):
+        if not self.root:
+            return
+        temp = self.root
+        while temp:
+            if temp.value == value:
+                return True
+            if value > temp.value and temp.right:
+                temp = temp.right
+            if value < temp.value and temp.left:
+                temp = temp.left
+        return False
+
+    def bfs_to_list(self):
+        temp = self.root
+        arr = list()
+        if temp:
+            q = deque()
+            q.append(temp)
+            while q:
+                elem = q.popleft()
+                arr.append(elem.value)
+                if elem.left:
+                    q.append(elem.left)
+                if elem.right:
+                    q.append(elem.right)
+        return arr
+
+
+tree = Tree()
+tree.root = TreeNode(5)
+tree.root.left = TreeNode(3)
+tree.root.left.left = TreeNode(2)
+tree.root.left.right = TreeNode(4)
+tree.root.right = TreeNode(8)
+tree.root.right.left = TreeNode(6)
+tree.root.right.right = TreeNode(9)
+
+arr = tree.bfs_to_list()
+print(arr)
