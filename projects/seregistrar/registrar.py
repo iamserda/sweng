4bc7366eeef789b81f2f58b7f4b397a3e6a29809
@iamserda <12580399+iamserda@users.noreply.github.com>
2025-03-18 22:04:48 -0400

    new project: registrar system: admin, student, instructor, course and grade -@iamserda

---
+++ projects/seregistrar/registrar.py
@@ -0,0 +1,64 @@
+from course import Course
+from people import *
+
+class Registrar:
+    count = 0
+    def __init__(self):
+        if Registrar.count == 0:
+            self.__courses = list()
+            self.__students = list()
+            self.__professors = list()
+            Registrar.count += 1
+        raise ValueError("Only 1 Registrar instance can exist!\nPlease use the existing one or delete it and create a new one.")
+
+    def create_course(self, name):
+        new_course = Course(name)
+        self.available_courses.append(new_course)
+        return new_course
+    
+    def hire_professor(self, first, last, address, dob):
+        new_professor = Professor(first, last, address, dob)
+        new_professor.salary = 200000
+        self.__professors.append(new_professor)
+        return new_professor
+    
+    def register_student(self, first, last, address, dob):
+        new_student = Student(first, last, address, dob)
+        self.__students.append(new_student)
+        return new_student
+    
+    def assign_professor_to_course(course:Course, professor:Professor):
+        if not course or not isinstance(course, Course):
+            print(TypeError("No course was provided, or invalid data type for course. course object must be a valid Course."))
+            return
+        
+        if not professor or not isinstance(professor, Professor):
+            print(TypeError("No course was provided, or invalid data type for course. course object must be a valid Course."))
+            return
+        
+        if course.instructor:
+            print(f"WARNING: Changing professor for {course.id} from Professor: {course.instructor} to professor{professor}.")
+        
+        if len(professor.courses) >= 4:
+            print(f"WARNING: Current professor: {course.instructor} will have more than the recommended limit. Message will be sent to Dean.")
+
+        course.instructor = professor
+        professor.courses.append(course.id)
+
+
+    def list_current_course(self):
+        course_offering = [course.id for course in courses]
+        return course_offering
+    
+    def delete_registra(self):
+        Registrar.count = 0
+        del self
+
+registrar1 = Registrar()
+try:
+    registrar2 = Registrar()
+except Exception as err:
+    print(err)
+    pass
+
+registrar2.create_course("PSY101: Introduction to Psychology")
