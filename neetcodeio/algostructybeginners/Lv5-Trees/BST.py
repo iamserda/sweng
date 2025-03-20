498eb9066f79e1a1dff8f7e4fbccaea2b6750256
@iamserda <12580399+iamserda@users.noreply.github.com>
2025-03-20 01:44:11 -0400

    Read over the code to gain some understanding of this. I will again try to implement it from scratch without checking the previous work. For now, i added new notes on the concepts of a BST. I am Feelin' the superpowers... La forca es conmigo -@iamserda

--- neetcodeio/algostructybeginners/Lv5-Trees/BST.py
+++ neetcodeio/algostructybeginners/Lv5-Trees/BST.py
@@ -80,16 +80,21 @@ class BinarySearchTree:
                 root.right = rm(root.right, value)
             elif value < root.value:
                 root.left = rm(root.left, value)
+            # if they are equal, which means I found the node to be removed.
             else:
-                # if they are equal, which means I found the node to be removed.
+                # if the node has no left subtree, we return the right
+                # if the node has no right subtree, we return the left
                 if not root.left:
                     return root.right
-                elif not root.right:
+                if not root.right:
                     return root.left
-                else:
-                    min_node = min_value_node(root.right)
-                    root.val = min_node.value
-                    root.right = rm(root.right, min_node.value)
+                # Otherwise:
+                # find the smallest node in the right subtree
+                min_node = min_value_node(root.right)
+                # replace root_val with min_node_val
+                root.val = min_node.value
+                # now remove the min node recursively
+                root.right = rm(root.right, min_node.value)
             return root
 
         self.root = rm(self.root, value)
@@ -104,15 +109,6 @@ my_bst.insert(6)
 my_bst.insert(2)
 my_bst.insert(14)
 my_bst.insert(10)
-print(my_bst.search(10))
 
-root = my_bst.root
-my_bst.remove(4)
-if root:
-    print(root.value)
-if root.left:
-    print(root.left.value)
-if root.right:
-    print(root.right.value)
-print(my_bst.search(-11))
-print(my_bst.search(10))
+for i in range(2, 16, 2):
+    print(f"Found {i}?: {my_bst.search(i)}")
