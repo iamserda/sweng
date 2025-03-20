cf5df2d3d5d9e90817366982235264cc781a1fca
@iamserda <12580399+iamserda@users.noreply.github.com>
2025-03-19 21:10:27 -0400

    minor: pylint fixes. -@iamserda

--- projects/seregistrar/grade.py
+++ projects/seregistrar/grade.py
@@ -1,19 +1,21 @@
+"""Grade module"""
+
 class Grade:
+    """creates grade objects, requires course_id(str), student_id(str), and a score(float)."""
+
     def __init__(self, course_id, student_id, score):
         self.course_id = course_id
         self.student_id = student_id
         self.score = score
         self.grade = None
+
     def __str__(self):
         return f"""course: {self.course_id}\nstudent: {self.student_id}\nscore: {self.score}\ngrade: {self.grade}"""
-    
+
     def get_grade(self):
+        """assigns a letter (A,B,C,D,F) for a given valid score."""
         score = self.score
-        grade_book = {
-            90: "A",
-            80: "B",
-            70: "A",
-            60: "D"}
+        grade_book = {90: "A", 80: "B", 70: "A", 60: "D"}
         if score < 0 or score > 100:
             print("invalid score, score cannot be less than 0 or more than 100")
             return
