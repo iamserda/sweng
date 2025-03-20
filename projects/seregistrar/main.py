ef077319cc7c13606fcb2f2f904b5526147eef86
@iamserda <12580399+iamserda@users.noreply.github.com>
2025-03-19 22:43:15 -0400

    wip: started working on the main workflow, for now, this app is mostly for an admin to hire new professor, create new courses, register students. Eventually, we will assign professors to courses as instructors. We will register students to courses as attendance. Finally, students will be receiving a  grade. -@iamserda

--- projects/seregistrar/main.py
+++ projects/seregistrar/main.py
@@ -3,20 +3,82 @@ from grade import Grade
 from course import Course
 from registrar import Registrar
 
-sole_registrar = Registrar()
+app_admin = Registrar()
 
-def get_info():
-    _data = {
-        "first":"",
-        "last":"",
-        "address":"",
-        "dob":""
-    }
+
+def get_person_info():
+    _data = {}
     _data["first"] = input("Enter First Name: ")
     _data["last"] = input("Enter Last Name: ")
     _data["address"] = input("Enter Address: ")
     _data["dob"] = input("Enter DOB: MM-DD-YYYY: ")
+    return _data
 
-def register_student():
-    
 
+def create_new_student():
+    student_info = get_person_info()
+    if student_info:
+        new_student = app_admin.hire_professor(**student_info)
+        print("Success: A new student was created.")
+        return new_student
+    return ValueError("Could not create new student with the info given.")
+
+
+def hire_new_professor():
+    professor_info = get_person_info()
+    if professor_info:
+        new_prof = app_admin.hire_professor(**professor_info)
+        print("Success: A new professor was created.")
+        return new_prof
+    return ValueError("Could not create new professor with the info given.")
+
+
+def create_new_course():
+    course_details = {}
+    course_details["title"] = input("Enter Course Title: ")
+    course_details["prof_id"] = input("Enter Professor ID: ")
+    if course_details["title"]:
+        new_course = app_admin.create_course(course_details["title"])
+        print(f"Sucess: A new course with title: {new_course.title} was created.")
+        if course_details["prof_id"] and new_course:
+            professor: Professor = app_admin.__professors[course_details["prof_id"]]
+            new_course.assign_instructor(professor)
+            print(
+                f"Sucess: Prof. {professor.first_name} {professor.last_name}  was assigned to this course."
+            )
+        return new_course
+    return ValueError("Could not create new course with the info given.")
+
+
+def display_options():
+    message = """Options:\n1- Create new course\n2- Add a new student\n3- Add a new Professor\n4- Show all courses\n9- Display this menu\nx- Exit app.\n\n"""
+    print(message)
+
+
+def get_all_courses():
+    for course in app_admin.list_current_course():
+        print(f"course:\ncourse\n")
+
+
+if __name__ == "__main__":
+    set_of_options = set(list("0123456789x"))
+    user_input = "1"
+    display_options()
+    while user_input.lower() in set_of_options:
+        user_input = input("$-> ")
+        match user_input:
+            case "1":
+                create_new_course()
+            case "2":
+                create_new_student()
+            case "3":
+                hire_new_professor()
+            case "4":
+                get_all_courses()
+            case "9":
+                display_options()
+            case "x":
+                break
+            case _:
+                print("Invalid options, please try again!\n")
+                user_input = "0"
