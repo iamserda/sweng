0783cedc843455198e6defa9268cda3a4282f86d
@iamserda <12580399+iamserda@users.noreply.github.com>
2025-03-21 02:06:23 -0400

    solved: bst iterative insertion, and manual tested -@iamserda

--- neetcodeio/algostructybeginners/Lv5-Trees/bst_practice1.py
+++ neetcodeio/algostructybeginners/Lv5-Trees/bst_practice1.py
@@ -27,9 +27,35 @@ class BinarySearchTree:
         self.root = inserter(self.root, value)
         return self.root
 
+    def insert_iterative(self, value):
+        new_node = Node(value)
+        if not self.root:
+            self.root = new_node
+        else:
+            temp = self.root
+            while temp:
+                if value > temp.value:
+                    if temp.right is None:
+                        temp.right = new_node
+                        return
+                    temp = temp.right
+                elif value < temp.value:
+                    if temp.left is None:
+                        temp.left = new_node
+                        return
+                    temp = temp.left
+                break
+
 
 my_bst = BinarySearchTree()
-my_bst.insert_recursive(8)
-my_bst.insert_recursive(12)
-my_bst.insert_recursive(4)
-print(my_bst.root, my_bst.root.left, my_bst.root.right)
+my_bst.insert_iterative(8)
+my_bst.insert_iterative(12)
+my_bst.insert_iterative(4)
+my_bst.insert_recursive(2)
+my_bst.insert_recursive(10)
+my_bst.insert_recursive(14)
+my_bst.insert_recursive(6)
+print(my_bst.root)
+print(my_bst.root.left, my_bst.root.right)
+print(my_bst.root.left.left, my_bst.root.left.right)
+print(my_bst.root.right.left, my_bst.root.right.right)
