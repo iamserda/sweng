91b83665c15ebf2a5a6237b55b577024367982bc
@iamserda <12580399+iamserda@users.noreply.github.com>
2025-03-20 22:56:44 -0400

    fixed: dll remove functionality updated, and tested -@iamserda

--- neetcodeio/algostructybeginners/Lv2-LinkedLists/doublyLinkedList.py
+++ neetcodeio/algostructybeginners/Lv2-LinkedLists/doublyLinkedList.py
@@ -58,7 +58,7 @@ class DoublyLinkedList:
         if index >= 0 and index < self.length:
             temp = self.head
             counter = 0
-            while temp.next:
+            while temp:
                 if counter + 1 == index:
                     new_node.next = temp.next
                     temp.next.prv = new_node
@@ -69,20 +69,70 @@ class DoublyLinkedList:
         return False
 
     def remove(self, index):
-        if index >= 0 and index < self.length:
-<<<<<<< HEAD
-            temp = self.head
-            counter = 0
-            while temp:
-                if counter == index:
-                    remove = temp
-                    temp = temp.next
-                    remove.prev
-=======
-            temp = self.head
-            counter = 0
-            while temp:
-                if counter == index:
-                    remove = temp
-                    temp = temp.next
-                    remove.prev
+        if index < 0 or index >= self.length:
+            return IndexError(f"Index: {index} is NOT VALID")
+        # remove head node
+        if index == 0:
+            removed = self.head
+            self.head = self.head.next
+            if self.head:
+                self.head.prev = None
+            removed.next = None
+            if self.tail == removed:
+                self.tail = None
+            self.length -= 1
+            return removed
+        temp = self.head
+        counter = 0
+        while temp and temp.next:
+            if counter + 1 == index:
+                removed = temp.next
+                temp.next = removed.next
+                if temp.next:
+                    temp.next.prev = temp
+                if removed == self.tail:
+                    self.tail = removed.prev
+                removed.prev = None
+                removed.next = None
+                self.length -= 1
+                return removed
+            temp = temp.next
+            counter += 1
+
+
+# TESTING ARENA:
+
+dll = DoublyLinkedList()
+dll.append(0)
+print(dll.get_values())
+dll.append(1)
+print(dll.get_values())
+dll.append(2)
+print(dll.get_values())
+dll.append(3)
+print(dll.get_values())
+dll.append(4)
+print(dll.get_values())
+dll.append(5)
+print(dll.get_values())
+
+
+def test_remove(index):
+    removed_node = dll.remove(index)
+    print(f"removed: {removed_node.val}")
+    print(dll.get_values())
+
+
+for i in range(-1, 7, 1):
+    removed_node = dll.remove(i)
+    if isinstance(removed_node, Node):
+        dll.prepend(f"x{i}")
+        print(f"removed: {removed_node.val}")
+        print(dll.get_values())
+    else:
+        print(removed_node)
+
+temp = dll.head
+while temp:
+    print(temp.val)
+    temp = temp.next
