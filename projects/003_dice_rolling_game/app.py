1980e02019abbec8d887b54bfe2315c7ffd6b122
@iamserda <12580399+iamserda@users.noreply.github.com>
2025-03-21 14:08:44 -0400

    renamed and moved

---
+++ projects/003_dice_rolling_game/app.py
@@ -0,0 +1,36 @@
+import random
+
+
+def start_game():
+    player1 = []
+    player2 = []
+    player1_turn = True
+    max_score = 21
+
+    while sum(player1) < 21 or sum(player2) < 21:
+        new_score = random.randrange(1, 6)
+        if player1_turn:
+            player1.append(new_score)
+            p1_score = sum(player1)
+            if p1_score == 21:
+                print("Player 1 wins!")
+                break
+            if p1_score > 21:
+                print("Player 2 wins!")
+                break
+        else:
+            player2.append(new_score)
+            p2_score = sum(player2)
+            if p2_score == 21:
+                print("Player 2 wins!")
+                break
+            if p2_score > 21:
+                print("Player 1 wins!")
+                break
+        player1_turn = not player1_turn
+
+    print(f"Player1: {p1_score} Player2: {p2_score}")
+
+
+if __name__ == "__main__":
+    start_game()
