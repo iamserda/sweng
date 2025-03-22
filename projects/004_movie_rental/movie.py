041789193d3fe488a08c5337f7bdb351a3fc8997
@iamserda <12580399+iamserda@users.noreply.github.com>
2025-03-21 23:06:18 -0400

    minor: formatting fix -@iamserda

--- projects/004_movie_rental/movie.py
+++ projects/004_movie_rental/movie.py
@@ -3,9 +3,9 @@ class Movie:
 
     def __init__(self, title: str, director: str) -> object:
         Movie.id_counter += 1
-        self.id = Movie.id_counter
-        self.title = title
-        self.director = director
+        self.id: int = Movie.id_counter
+        self.title: str = title
+        self.director: str = director
 
     def __str__(self):
         return f"id: {self.id}, title: {self.title}, director: {self.director}"
