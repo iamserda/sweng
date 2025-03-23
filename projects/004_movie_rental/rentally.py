0e535d5a64d03d4d6446e24c176aa839583fa3c5
@iamserda <12580399+iamserda@users.noreply.github.com>
2025-03-23 19:01:35 -0400

    minor: reordered some functions to make it easier to search, ordered alpha. -@iamserda

--- projects/004_movie_rental/rentally.py
+++ projects/004_movie_rental/rentally.py
@@ -25,15 +25,6 @@ class Rentally:
             self.customers[customer.customer_id] = customer
             # print(f"Sucess: Created a new customer with id: {customer.customer_id}")
 
-    def remove_customer(self):
-        customer = self.get_customer()
-        if isinstance(customer, Customer):
-            self.customers.pop(customer.customer_id)
-            print(f"Sucess: Removed a new customer with id: {customer.customer_id}")
-        else:
-            print(f"Failure: No customer found with given ID")
-
-    # MOVIE METHODS
     def add_movie(self, title, director=None):
         new_movie = Movie(title, director)
         if new_movie.id in self.library:
@@ -44,6 +35,29 @@ class Rentally:
         self.available_movies.add(new_movie.id)
         return new_movie.id
 
+    def get_customer(self):
+        customer_id = int(input("Enter Customer ID: "))
+        customer = self.customers.get(customer_id)
+        if not isinstance(customer, Customer):
+            print(f"A Customer with id: {customer_id} was not found!")
+        return customer
+
+    def get_movie_id(self) -> int:
+        movie_id = int(input("Enter Movie ID: "))
+        if movie_id in self.rented_movies:
+            return movie_id
+        return -1
+
+    def remove_customer(self):
+        customer = self.get_customer()
+        if isinstance(customer, Customer):
+            self.customers.pop(customer.customer_id)
+            print(f"Sucess: Removed a new customer with id: {customer.customer_id}")
+        else:
+            print(f"Failure: No customer found with given ID")
+
+    # MOVIE METHODS
+
     def remove_movie(self, movie_id):
         if movie_id in self.rented_movies:
             print(f"Cannot remove {movie_id} from system because it is rented.")
@@ -52,11 +66,6 @@ class Rentally:
                 del self.available_movies[movie_id]
             del self.library[movie_id]
             print(f"Sucess: {movie_id} was deleted from library.")
-
-    def search_by_title(self, movie_title):
-        for movie in self.library.values():
-            if movie_title == movie.title:
-                return movie
 
     def rent_a_movie(self):
         print("\nRent-A-Movie:")
@@ -85,17 +94,17 @@ class Rentally:
         try:
             movie_id = int(input("Enter Movie ID: "))
             customer = self.get_customer()
-
             if movie_id not in self.rented_movies:
-                raise KeyError(f"Error: Movie ID: {movie_id} is NOT in our records.")
-
+                raise KeyError(
+                    f"Error: Movie ID: {movie_id} is NOT in our Rented records."
+                )
             if customer.customer_id != self.rented_movies.get(movie_id):
                 raise KeyError(
                     "Error: This customer ID does not match our records for this rental."
                 )
             customer.rented.remove(movie_id)
-            returned_movie = self.rented_movies.pop(movie_id)
-            self.available_movies[movie_id] = returned_movie
+            self.rented_movies.pop(movie_id)
+            self.available_movies.add(movie_id)
             print(
                 f"Success: Customer: {customer.customer_id} has successfully returned movie: {movie_id}. Thank you!"
             )
@@ -104,15 +113,7 @@ class Rentally:
         except Exception as err:
             print(err)
 
-    def get_customer(self):
-        customer_id = int(input("Enter Customer ID: "))
-        customer = self.customers.get(customer_id)
-        if not isinstance(customer, Customer):
-            print(f"A Customer with id: {customer_id} was not found!")
-        return customer
-
-    def get_movie_id(self) -> int:
-        movie_id = int(input("Enter Movie ID: "))
-        if movie_id in self.rented_movies:
-            return movie_id
-        return -1
+    def search_by_title(self, movie_title):
+        for movie in self.library.values():
+            if movie_title == movie.title:
+                return movie
