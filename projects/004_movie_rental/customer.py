02ac1be166225cf5c1e10f62a54ac4f45f5c1a19
@iamserda <12580399+iamserda@users.noreply.github.com>
2025-03-26 21:06:06 -0400

    minor: add saving feature to save each newly created customer to a pre-exisiting customer_db.csv file.

--- projects/004_movie_rental/customer.py
+++ projects/004_movie_rental/customer.py
@@ -1,3 +1,5 @@
+import csv
+
 class Customer:
     # class props
     _id = 0
@@ -11,11 +13,19 @@ class Customer:
     def __str__(self):
         return f"id: {self.customer_id} -- name: {self.name}"
 
+    def as_dict(self):
+        return {"id": self.customer_id, "name": self.name, "rented": list(self.rented)}
+
     def check_rented(self, movie_id):
         return movie_id in self.rented
 
     def get_rented(self):
         return self.rented
+
+    def save_to_db(self):
+        with open("./customer_db.csv", "a+", newline="") as cust_db:
+            writer = csv.DictWriter(cust_db)
+            writer.writerow(cust_db.as_dict())
 
 
 if __name__ == "__main__":
