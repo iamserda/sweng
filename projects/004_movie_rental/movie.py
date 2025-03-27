a1f8bc15a88762159a3ad6559e841dc8417c184f
@iamserda <12580399+iamserda@users.noreply.github.com>
2025-03-26 21:09:57 -0400

    new: feature -> when a new Movie is added to the database, we will save it to a csv file for permanet storage -@iamserda

--- projects/004_movie_rental/movie.py
+++ projects/004_movie_rental/movie.py
@@ -1,3 +1,5 @@
+import csv
+
 class Movie:
     id_counter = 0
 
@@ -9,3 +11,11 @@ class Movie:
 
     def __str__(self):
         return f"id: {self.id}, title: {self.title}, director: {self.director}"
+
+    def movie_as_dict(self):
+        return {"id": self.id, "title": self.title, "director": self.director}
+
+    def save_to_db(self):
+        with open("movie_db.csv", "a+", newline="") as movie_db:
+            writer = csv.DictWriter(movie_db)
+            writer.writerow(movie_db.as_dict())
