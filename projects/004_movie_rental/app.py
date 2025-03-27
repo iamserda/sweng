d482dc404dfd078b16585f63bcadac6d2dcdbc1a
@iamserda <12580399+iamserda@users.noreply.github.com>
2025-03-27 15:34:24 -0400

    fix: renamed method start_game to start, externalized some functionalities to clean up the start menthod, method now takes an arg, an rentally-obj. -@iamserda

--- projects/004_movie_rental/app.py
+++ projects/004_movie_rental/app.py
@@ -5,11 +5,28 @@ from populate_data import test_populate
 
 
 def show_menu():
-    message = "\n1. Rent a Movie\n2. Return a Movie\n3. Add a Movie\n4. Delete a Movie\n5. Add a Customer\n6. Delete a Customer\nm. Show Menu\nx. Exit\n"
+    message = "\n1. Rent a Movie\n2. Return a Movie\n3. Add a Movie\n4. Delete a Movie\n5. Add a Customer\n6. Delete a Customer\nm. Show Menu\np. Populate Databases\nx. Exit\n"
     print(message)
 
 
-def start():
+def add_new_movie():
+    movie_title = input("Enter movie title: ")
+    movie_director = input("Enter movie director's name: ")
+    system.add_movie(title=movie_title, director=movie_director)
+
+
+def delete_a_movie():
+    print("WARNING: Selected 'Delete a Movie'")
+    movie_id = int(input("Enter movie ID: "))
+    system.remove_movie(movie_id)
+
+
+def add_new_customer():
+    customer_name = input("Enter Customer's name: ")
+    system.add_customer(name=customer_name)
+
+
+def start(system: Rentally):
     show_menu()
     flag = True
     while flag:
@@ -20,16 +37,11 @@ def start():
             case "2":
                 system.return_a_movie()
             case "3":
-                movie_title = input("Enter movie title: ")
-                movie_director = input("Enter movie director's name: ")
-                system.add_movie(title=movie_title, director=movie_director)
+                add_new_movie()
             case "4":
-                print("WARNING: Selected 'Delete a Movie'")
-                movie_id = int(input("Enter movie ID: "))
-                system.remove_movie(movie_id)
+                delete_a_movie()
             case "5":
-                customer_name = input("Enter Customer's name: ")
-                system.add_customer(name=customer_name)
+                add_new_customer()
             case "6":
                 system.remove_customer()
             case "m":
@@ -43,4 +55,4 @@ def start():
 if __name__ == "__main__":
     print("\nWelcome to Rentally:\n")
     system = Rentally()
-    start()
+    start(system)
