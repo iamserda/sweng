42c059c45a5b38e867b852992c90e6247d23e521
@iamserda <12580399+iamserda@users.noreply.github.com>
2025-03-27 13:40:56 -0400

    fix: path issues -@iamserda

--- projects/004_movie_rental/movie.py
+++ projects/004_movie_rental/movie.py
@@ -16,6 +16,6 @@ class Movie:
         return {"id": self.id, "title": self.title, "director": self.director}
 
     def save_to_db(self):
-        with open("movie_db.csv", "a+", newline="") as movie_db:
+        with open("./data/movie_db.csv", "a+", newline="") as movie_db:
             writer = csv.DictWriter(movie_db, fieldnames=["id", "title", "director"])
             writer.writerow(self.as_dict())
