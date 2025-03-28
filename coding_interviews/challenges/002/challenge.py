460300876a27bd5c931151c469b6275d94af6d20
@iamserda <12580399+iamserda@users.noreply.github.com>
2025-03-28 01:34:36 -0400

    setup challenge -@iamserda

---
+++ coding_interviews/challenges/002/challenge.py
@@ -0,0 +1,35 @@
+"""
+Challege:
+Most Frequently Occurring Item in an Array (Python)
+Find the most frequently occurring item in an array.
+
+Example: The most frequently occurring item in [1, 3, 1, 3, 2, 1] is 1.
+
+If you're given an empty array, you should return null (in Java) or None (in Python).
+
+You can assume that there is always a single, unique value that appears most frequently unless the array is empty.  For instance, you won't be given an array such as [1, 1, 2, 2].
+
+NOTE: We're going to use lists instead of arrays in Python for simplicity.
+"""
+
+
+# Implement your function below.
+def most_frequent(given_list):
+    pass
+
+
+# NOTE: The following input values will be used for testing your solution.
+# most_frequent(list1) should return 1
+assert most_frequent([1, 3, 1, 3, 2, 1]) == 1
+
+# most_frequent(list2) should return 3
+assert most_frequent([3, 3, 1, 3, 2, 1]) == 3
+
+# most_frequent(list3) should return None
+assert most_frequent([]) == None
+
+# most_frequent(list4) should return 0
+assert most_frequent([0]) == 0
+
+# most_frequent(list5) should return -1
+assert most_frequent([0, -1, 10, 10, -1, 10, -1, -1, -1, 1]) == -1
