32b1ec1fad175d950ef3bc75ba7d5d8f6e7596f8
@iamserda <12580399+iamserda@users.noreply.github.com>
2025-03-28 23:42:13 -0400

    solution1: path_from_leaf , which is basically some form of in-order traversal with back-pedaling when a path does not lead to a leaf node. Once we find a leaf node, we start populating the array with the nodes from leaf to root. -@iamserda

--- neetcodeio/algostructybeginners/Lv6-BacktrackingDFS/getpath/solution.py
+++ neetcodeio/algostructybeginners/Lv6-BacktrackingDFS/getpath/solution.py
@@ -22,7 +22,18 @@ def path_from_root(root: TreeNode, k: int, path: list) -> list:
 
 def path_from_leaf(root: TreeNode, k: int, path: list) -> list:
     """Find a path [a,..,z] where k is not the path in a give Binary Tree"""
-    pass
+    if root is None or root.value == k:
+        return False
+
+    if (
+        (root.left is None and root.right is None)
+        or path_from_leaf(root.left, k, path)
+        or path_from_leaf(root.right, k, path)
+    ):
+        path.append(root.value)
+        return True
+
+    return False
 
 
 root = TreeNode(5)
@@ -50,10 +61,9 @@ def test1(root, k, path_):
 def test2(root, k, path_):
     print("Test #2:")
     if path_from_leaf(root, k, path_):
-        print("Path FOUND:", path_)
+        print("FOUND: leaf->", path_, "<-root")
     else:
         print("Path NOT FOUND!")
-
 
 test1(root, 3, [])
 test2(root, 3, [])
