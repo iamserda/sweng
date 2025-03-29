017cc19d7d7e135c3ba522009648cd139e73bba1
@iamserda <12580399+iamserda@users.noreply.github.com>
2025-03-28 23:48:27 -0400

    setup: challenge for finding a path from root to any leaf node. The path cannot contain any node with a value k. Backtracking Basics -@iamserda

---
+++ neetcodeio/algostructybeginners/Lv6-BacktrackingDFS/haspath/challenge.py
@@ -0,0 +1,45 @@
+class TreeNode:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+
+
+def has_path_without_K(root: TreeNode, k: int) -> bool:
+    """Returns True of a path was found without K"""
+    pass
+
+
+# TESTING ARENAS:
+
+root = TreeNode(5)
+root.left = TreeNode(6)
+root.left.left = TreeNode(3)
+root.left.right = TreeNode(3)
+root.left.left.left = TreeNode(20)
+root.left.left.right = TreeNode(32)
+root.left.right.left = TreeNode(2)
+root.right = TreeNode(7)
+root.right.left = TreeNode(9)
+root.right.right = TreeNode(8)
+root.right.left.right = TreeNode(30)
+root.right.right.right = TreeNode(3)
+path_: list = []
+
+print("FOUND!" if has_path_without_K(root, 3) else "NOT FOUND!")  # FOUND
+
+root = TreeNode(5)
+root.left = TreeNode(6)
+root.left.left = TreeNode(3)
+root.left.right = TreeNode(3)
+root.left.left.left = TreeNode(20)
+root.left.left.right = TreeNode(32)
+root.left.right.left = TreeNode(2)
+root.right = TreeNode(7)
+root.right.left = TreeNode(3)
+root.right.right = TreeNode(8)
+root.right.left.right = TreeNode(30)
+root.right.right.right = TreeNode(3)
+path_: list = []
+
+print("FOUND!" if has_path_without_K(root, 3) else "NOT FOUND!")  # NOT FOUND
