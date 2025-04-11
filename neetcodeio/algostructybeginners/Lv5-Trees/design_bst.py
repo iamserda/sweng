42dd88c018462a94071b5ab25e37083b28679880
@iamserda <12580399+iamserda@users.noreply.github.com>
2025-04-10 22:55:37 -0400

    init: setup challenge and TDD-tests. @-iamserda

---
+++ neetcodeio/algostructybeginners/Lv5-Trees/design_bst.py
@@ -0,0 +1,62 @@
+class Node:
+    def __init__(self, key, val):
+        pass
+
+    def __str__(self):
+        return f"key:{self.key}, val: {self.val}"
+
+
+class TreeMap:
+    def __init__(self):
+        pass
+
+    def insert(self, key, val) -> None:
+        pass
+
+    def get(self, key: int) -> int:
+        pass
+
+    def getMin(self) -> int:
+        """return the value mapped to the SMALLEST key in the tree.
+        If the tree is empty, return -1."""
+        pass
+
+    def getMax(self) -> int:
+        """Returns the value mapped to the largest key in the tree.
+        If the tree is empty, return -1."""
+        pass
+
+    def remove(self, key) -> None:
+        """Removes the key-value pair with a given key located from the tree."""
+        pass
+
+    def dfs(root, arr):
+        pass
+
+    def getInorderKeys(self) -> list[int]:
+        """Returns an list of the keys in the tree in ascending order."""
+        pass
+
+
+# Test-Driven Development
+new_tmap = TreeMap()
+new_tmap.insert("Ronaldo", 1976)
+new_tmap.insert("Jordan", 1963)
+new_tmap.insert("Bryant", 1978)
+new_tmap.insert("Messi", 1987)
+assert new_tmap.getInorderKeys() == ["Bryant", "Jordan", "Messi", "Ronaldo"]
+assert new_tmap.get("Ronaldo") == 1976
+assert new_tmap.get("Messi") == 1987
+assert new_tmap.get("Jordan") == 1963
+# Kobe's Year of birth, because (B) in key 'Bryant' is first when ordered from left-to-right
+assert new_tmap.getMin() == 1978
+# Kobe's Year of birth, because (R) in key 'Ronaldo' is first when ordered from left-to-right
+assert new_tmap.getMax() == 1976
+new_tmap.remove("Bryant")
+assert new_tmap.getInorderKeys() == ["Jordan", "Messi", "Ronaldo"]
+new_tmap.remove("Jordan")
+assert new_tmap.getInorderKeys() == ["Messi", "Ronaldo"]
+new_tmap.remove("Messi")
+assert new_tmap.getInorderKeys() == ["Ronaldo"]
+new_tmap.remove("Ronaldo")
+assert new_tmap.getInorderKeys() == []
