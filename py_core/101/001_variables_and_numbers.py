6936af6469a03c5c6c39d72f2b5cd16eac72b524
@iamserda <12580399+iamserda@users.noreply.github.com>
2025-04-21 18:54:19 -0400

    minor: getting back in the groove of things after a few days of break. Just coding for the fun of it. Let's Get it! -@iamserda

--- py_core/101/001_variables_and_numbers.py
+++ py_core/101/001_variables_and_numbers.py
@@ -1,9 +1,18 @@
-""" integer or counting numbers"""
-age = 20
-print("gpa: {} has a data type of `{}`".format(
-    age, str(type(age)).split("'")[1]))
-
-# real numbers or floating-point numbers
-gpa = 3.0
-print("gpa: {} has a data type of `{}`".format(
-    gpa, str(type(gpa)).split("'")[1]))
+# Variables
+name = "Ronaldo Nazario aka O'Phenomenon"
+birth_year = 1976
+world_cup_year = 1994
+age = world_cup_year - birth_year
+print(
+    f"{name} was {age} years old when he won his first FIFA World Cup with Brazil in {world_cup_year}!"
+)
+world_cup_year = 1998
+age = world_cup_year - birth_year
+print(
+    f"{name} was {age} years old when he lost the FIFA World Cup with Brazil in {world_cup_year}!"
+)
+world_cup_year = 2002
+age = world_cup_year - birth_year
+print(
+    f"{name} was {age} years old when he won the FIFA World Cup with Brazil in {world_cup_year}"
+)
