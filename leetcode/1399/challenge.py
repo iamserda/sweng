982fda3018303e31fb4de5a6d04a1e81eeecfb48
@iamserda <12580399+iamserda@users.noreply.github.com>
2025-04-23 23:30:47 -0400

    Setup LeetCode 1399 -@iamserda

---
+++ leetcode/1399/challenge.py
@@ -0,0 +1,9 @@
+class Solution:
+    def countLargestGroup(self, n: int) -> int:
+        pass
+
+
+sol = Solution()
+assert sol.countLargestGroup(13) == 4
+assert sol.countLargestGroup(2) == 1
+assert sol.countLargestGroup(173) == 1
