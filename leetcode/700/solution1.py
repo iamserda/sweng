a1724d75bcb243aa74fb7726608414d195e45477
@iamserda <12580399+iamserda@users.noreply.github.com>
2025-04-26 19:56:59 -0400

    solution: leetcode 700, iterative binary search on a bst. -@iamserda

---
+++ leetcode/700/solution1.py
@@ -0,0 +1,37 @@
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def searchBST(self, root, val):
+        temp = root
+        while isinstance(temp, TreeNode):
+            if val == temp.val:
+                return True
+            elif val > temp.val:
+                temp = temp.right
+            elif val < temp.val:
+                temp = temp.left
+
+
+bst = TreeNode(
+    5,
+    left=TreeNode(3, left=TreeNode(1), right=TreeNode(4)),
+    right=TreeNode(7, left=TreeNode(6), right=TreeNode(8)),
+)
+
+sol = Solution()
+assert sol.searchBST(bst, 2) == None or False
+assert sol.searchBST(bst, 0) == None or False
+assert sol.searchBST(bst, 9) == None or False
+assert sol.searchBST(bst, 1) == True
+assert sol.searchBST(bst, 3) == True
+assert sol.searchBST(bst, 4) == True
+assert sol.searchBST(bst, 5) == True
+assert sol.searchBST(bst, 6) == True
+assert sol.searchBST(bst, 7) == True
+assert sol.searchBST(bst, 8) == True
