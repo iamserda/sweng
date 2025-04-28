7d37057f92faf4de2345f8363fd9bfe8425023f3
@iamserda <12580399+iamserda@users.noreply.github.com>
2025-04-27 21:01:23 -0400

    init: challenge write a binary search tree with the following features, insert iteratively, insert recursively, remove. Finally provide a level_order method that returns the values of earch node at a given level as an array, so it should be a 2d array where each sub-array contains the value for a given level. [[]...] len of the array is the same as the height of the tree.  -@iamserda

---
+++ neetcodeio/algostructybeginners/Lv5-Trees/bst_practice2.py
@@ -0,0 +1,56 @@
+class TreeNode:
+    def __init__(self, val):
+        pass
+
+
+class BinarySearchTree:
+    def __init__(self):
+        pass
+
+    def insert(self, val):
+        pass
+
+    def insert_recursive(self, val):
+        pass
+
+    def remove(self, val):
+        pass
+
+    def level_order_traversal(self):
+        pass
+
+
+# Testing Arenas:
+bst = BinarySearchTree()
+bst.insert(4)
+bst.insert(2)
+bst.insert(4)
+bst.insert(6)
+assert bst.root.val == 4
+assert bst.root.left.val == 2
+assert bst.root.right.val == 6
+bst.remove(6)
+print(bst.level_order_traversal())
+
+
+# Testing Arenas:
+bst_two = BinarySearchTree()
+bst_two.insert_recursive(4)
+bst_two.insert_recursive(2)
+bst_two.insert_recursive(6)
+assert bst_two.root.val == 4
+assert bst_two.root.left.val == 2
+assert bst_two.root.right.val == 6
+print(bst_two.level_order_traversal())
+
+bst_two.remove(4)
+assert bst_two.root.val == 6
+assert bst_two.root.left.val == 2
+assert bst_two.root.right.val == None
+print(bst_two.level_order_traversal())
+
+bst_two.remove(6)
+assert bst_two.root.val == 2
+assert bst_two.root.left.val == None
+assert bst_two.root.right.val == None
+print(bst_two.level_order_traversal())
