bc09a6a6e2925217d2008dcc5d62f6aa847fda8a
@iamserda <12580399+iamserda@users.noreply.github.com>
2025-04-27 21:09:06 -0400

    feat: added iterative insertion and tested -@iamserda

--- neetcodeio/algostructybeginners/Lv5-Trees/bst_practice2.py
+++ neetcodeio/algostructybeginners/Lv5-Trees/bst_practice2.py
@@ -1,14 +1,35 @@
 class TreeNode:
     def __init__(self, val):
-        pass
+        self.val = val
+        self.left = None
+        self.right = None
 
 
 class BinarySearchTree:
     def __init__(self):
-        pass
+        self.root = None
 
     def insert(self, val):
-        pass
+        new_node = TreeNode(val)
+        if not self.root:
+            self.root = new_node
+        else:
+            temp = self.root
+            while temp:
+                if val > temp.val:
+                    if temp.right is None:
+                        temp.right = new_node
+                        break
+                    temp = temp.right
+                    continue
+                if val < temp.val:
+                    if temp.left is None:
+                        temp.left = new_node
+                        break
+                    temp = temp.left
+                    continue
+                break
+        return self.root
 
     def insert_recursive(self, val):
         pass
@@ -29,28 +50,28 @@ bst.insert(6)
 assert bst.root.val == 4
 assert bst.root.left.val == 2
 assert bst.root.right.val == 6
-bst.remove(6)
-print(bst.level_order_traversal())
+# bst.remove(6)
+# print(bst.level_order_traversal())
 
 
-# Testing Arenas:
-bst_two = BinarySearchTree()
-bst_two.insert_recursive(4)
-bst_two.insert_recursive(2)
-bst_two.insert_recursive(6)
-assert bst_two.root.val == 4
-assert bst_two.root.left.val == 2
-assert bst_two.root.right.val == 6
-print(bst_two.level_order_traversal())
+# # Testing Arenas:
+# bst_two = BinarySearchTree()
+# bst_two.insert_recursive(4)
+# bst_two.insert_recursive(2)
+# bst_two.insert_recursive(6)
+# assert bst_two.root.val == 4
+# assert bst_two.root.left.val == 2
+# assert bst_two.root.right.val == 6
+# print(bst_two.level_order_traversal())
 
-bst_two.remove(4)
-assert bst_two.root.val == 6
-assert bst_two.root.left.val == 2
-assert bst_two.root.right.val == None
-print(bst_two.level_order_traversal())
+# bst_two.remove(4)
+# assert bst_two.root.val == 6
+# assert bst_two.root.left.val == 2
+# assert bst_two.root.right.val == None
+# print(bst_two.level_order_traversal())
 
-bst_two.remove(6)
-assert bst_two.root.val == 2
-assert bst_two.root.left.val == None
-assert bst_two.root.right.val == None
-print(bst_two.level_order_traversal())
+# bst_two.remove(6)
+# assert bst_two.root.val == 2
+# assert bst_two.root.left.val == None
+# assert bst_two.root.right.val == None
+# print(bst_two.level_order_traversal())
