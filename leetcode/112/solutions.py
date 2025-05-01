9e1a6803bbe6911444535fd075602c5c8c319bac
@iamserda <12580399+iamserda@users.noreply.github.com>
2025-05-01 01:02:52 -0400

    solved: used dfs to solve 112. -@iamserda

--- leetcode/112/solutions.py
+++ leetcode/112/solutions.py
@@ -8,4 +8,38 @@ class TreeNode:
 
 class Solution:
     def hasPathSum(self, root: list[TreeNode], targetSum: int) -> bool:
-        pass
+        if not root:
+            return False
+        new_target_sum = targetSum - root.val
+        if root.left is None and root.right is None:
+            return new_target_sum == 0
+        return self.hasPathSum(root.left, new_target_sum) or self.hasPathSum(
+            root.right, new_target_sum
+        )
+
+
+sol = Solution()
+
+tree = TreeNode(5)
+tree.left = TreeNode(4)
+tree.left.left = TreeNode(11)
+tree.left.left.left = TreeNode(7)
+tree.left.left.right = TreeNode(2)
+tree.right = TreeNode(8)
+tree.right.left = TreeNode(13)
+tree.right.right = TreeNode(4)
+tree.right.right.right = TreeNode(1)
+target = 22
+assert sol.hasPathSum(tree, target) == True
+
+tree = TreeNode(1)
+tree.left = TreeNode(2)
+tree.right = TreeNode(3)
+target = 5
+assert sol.hasPathSum(tree, target) == False
+
+
+tree = TreeNode(1)
+tree.left = TreeNode(2)
+target = 0
+assert sol.hasPathSum(tree, target) == False
