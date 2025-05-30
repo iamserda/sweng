0d35a342b196c036fb2eaa93d9af466d9c413a81
@iamserda <12580399+iamserda@users.noreply.github.com>
2025-05-29 23:06:55 -0400

    minor: setting some attributes of the library systems to ''private''

--- py_oop/libraryms.py
+++ py_oop/libraryms.py
@@ -1,33 +1,34 @@
 class Library:
-    def __init__(self):
-        self.available_books = []
-        self.borrowed_books = []
-        self.current_acconts = []
-        self.closed_accounts = []
+    def __init__(self, name="Simus Library System"):
+        self.library_name = name
+        self.__available_books = []
+        self.__borrowed_books = []
+        self.__current_acconts = []
+        self.__closed_accounts = []
 
     def add_book(self, title: str, author: str = "", count: int = 1):
-        for index, book in enumerate(self.available_books):
+        for index, book in enumerate(self.__available_books):
             if title == book["title"]:
-                self.available_books[index]["count"] += count
+                self.__available_books[index]["count"] += count
                 break
         else:
             new_book = {"title": title, "author": author, "count": count, "borrowed": 0}
-            self.available_books.append(new_book)
+            self.__available_books.append(new_book)
 
     def remove_book(self, title: str) -> bool:
-        for i, book in enumerate(self.available_books):
+        for i, book in enumerate(self.__available_books):
             if title == book["title"]:
                 print(f"Deleting book: {book} from Library.")
-                self.available_books = (
-                    self.available_books[:i] + self.available_books[i + 1 :]
+                self.__available_books = (
+                    self.__available_books[:i] + self.__available_books[i + 1 :]
                 )
                 return True
         return False
 
     def borrow_book(self, title):
-        if not self.available_books:
+        if not self.__available_books:
             print(f"There are no books in this library system.")
-        for book in self.available_books:
+        for book in self.__available_books:
             if book["title"] == title:
                 if book["count"] < 1:
                     print(
@@ -37,28 +38,32 @@ class Library:
                     return
                 book["count"] -= 1
                 book["borrowed"] = 1
-                self.borrowed_books.append(title)
+                self.__borrowed_books.append(title)
                 print("success")
 
     def return_book(self, title):
         try:
-            index = self.borrowed_books.index(title)
-            del self.borrowed_books[index]
-            for book in self.available_books:
+            index = self.__borrowed_books.index(title)
+            for book in self.__available_books:
                 if book["title"] == title:
                     book["count"] += 1
-            print("Message: Return accepted! Thank you!")
-            print(f"Logging: Accepted return for title: {title}")
+                    del self.__borrowed_books[index]
+                    print("Message: Return accepted! Thank you!")
+                    print(f"Logging: Accepted return for title: {title}")
+                    break
         except ValueError as err:
             print("Message: This title was not borrowed from our library.")
             print(f"Logging: {err}")
 
     def display_books(self):
-        print(f"\tID#\t\tTitle:\t\tAuthor:\t\tCopies:")
-        for index, book in enumerate(self.available_books):
+        print(f"\n\tID#\t\tTitle:\t\tAuthor:\t\tCopies:")
+        for index, book in enumerate(self.__available_books):
             print(
                 f"\t{index + 1}.\t\t{book['title']}\t\t{book['author']}\t\t{book['count']}"
             )
+
+    def display_library_info(self):
+        print(self.library_name)
 
     def create_customer_account(self):
         pass
@@ -74,6 +79,7 @@ def show_menu():
 
 def start():
     lib_admin = Library()
+    lib_admin.display_library_info()
     show_menu()
     while True:
         user_selected = input("{-: ")
@@ -93,25 +99,25 @@ def start():
                     new_book = {"title": title, "author": author, "count": count}
                     lib_admin.add_book(title=title, author=author, count=count)
                     print("Message: Sucessfully added a new book.")
-                    if lib_admin.available_books:
+                    if lib_admin.__available_books:
                         lib_admin.display_books()
             case 3:
                 print("Removing a book:")
                 title = input("Enter the book's title: ")
                 lib_admin.remove_book(title)
-                if lib_admin.available_books:
+                if lib_admin.__available_books:
                     lib_admin.display_books()
             case 4:
                 print("Borrowing a book:")
                 title = input("Enter the book's title: ")
                 lib_admin.borrow_book(title)
-                if lib_admin.available_books:
+                if lib_admin.__available_books:
                     lib_admin.display_books()
             case 5:
                 print("Returning a book:")
                 title = input("Enter the book's title: ")
-                lib_admin.remove_book(title)
-                if lib_admin.available_books:
+                lib_admin.return_book(title)
+                if lib_admin.__available_books:
                     lib_admin.display_books()
             case "x":
                 return
