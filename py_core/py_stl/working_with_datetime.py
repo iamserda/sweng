02a01d9dc86a836e270dd5d7bd2d3a861ac8a248
@iamserda <12580399+iamserda@users.noreply.github.com>
2025-06-18 17:21:37 -0400

    working with datetime module basics -@iamserda

---
+++ py_core/py_stl/working_with_datetime.py
@@ -0,0 +1,8 @@
+from datetime import datetime
+
+current_datetime = datetime.now()
+current_date = current_datetime.date()
+current_time = current_datetime.time()
+print(current_date)
+print(current_time)
+print(current_datetime)
