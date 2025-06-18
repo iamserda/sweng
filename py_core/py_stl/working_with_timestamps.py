e3ae285fb796e827e40765350635d337a01368cd
@iamserda <12580399+iamserda@users.noreply.github.com>
2025-06-18 17:02:26 -0400

    minor: using 10M loop cycles instead of 1B -@iamserda

--- py_core/py_stl/working_with_timestamps.py
+++ py_core/py_stl/working_with_timestamps.py
@@ -10,4 +10,4 @@ end_time = time.time()
 time_span = end_time - start_time
 print(f"started_at: {time.ctime(start_time)}")
 print(f"finished_at: {time.ctime(end_time)}")
-print(f"elapsed_time: {"{}".format(round(time_span, 2), )}")
+print(f"elapsed_time: {"{}".format(round(time_span, 2), )} seconds.")
