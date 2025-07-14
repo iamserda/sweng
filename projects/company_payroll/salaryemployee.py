d3df652e07dd280a949f9f86f9fbafa24afcb7e5
@iamserda <12580399+iamserda@users.noreply.github.com>
2025-07-14 00:19:02 -0400

    minor: added protection from executing parts of the module during imports. Updates class attribute SALARY_EXPENSE when bonuse pay is calculated -@iamserda

--- projects/company_payroll/salaryemployee.py
+++ projects/company_payroll/salaryemployee.py
@@ -21,22 +21,27 @@ class SalaryEmployee(Employee):
             self.current_bonus_pay = 0
         else:
             self.current_bonus_pay = self.base_salary * (bonus_rate / 100)
-            self.ytd_bonus_pay += self.current_bonus_pay
-            self.ytd_gross_pay += self.current_bonus_pay
+
+        self.ytd_bonus_pay += self.current_bonus_pay
+        self.ytd_gross_pay += self.current_bonus_pay
+        Employee.SALARY_EXPENSE += self.current_bonus_pay
 
     def calculate_gross_pay(self):
         self.gross_pay = self.base_salary / 52
         self.ytd_gross_pay += self.gross_pay
+        Employee.SALARY_EXPENSE += self.gross_pay
 
 
-sal_emp1 = SalaryEmployee("Lara", "Croft", 210000)
+if __name__ == "__main__":
+    sal_emp1 = SalaryEmployee("Lara", "Croft", 210000)
 
-sal_emp1.hours_worked = 50.00
-sal_emp1.hourly_rate = 93.87
-for i in range(52):
-    # weekly salary based on 45 hours
-    sal_emp1.calculate_gross_pay()
-    if i == 51:
-        sal_emp1.calculate_bonus_pay(20)
+    sal_emp1.hours_worked = 50.00
+    sal_emp1.hourly_rate = 93.87
+    for i in range(52):
+        # weekly salary based on 45 hours
+        sal_emp1.calculate_gross_pay()
+        if i == 51:
+            sal_emp1.calculate_bonus_pay(20)
 
-print(sal_emp1)
+    print(sal_emp1)
+    print(f"Total Salary Expense: {Employee.SALARY_EXPENSE}")
