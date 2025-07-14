4631ae9c9344801dfdf0797ea2a339c41545d277
@iamserda <12580399+iamserda@users.noreply.github.com>
2025-07-14 15:08:18 -0400

    fixed dunder method str to better present checks, made some changes to how grosspay is calculated, separated regular pay(under 40hrs), overtime pay(over 40hrs), finally new limit to bonus, bonuses are earned only regular pay and not ot-pay -@iamserda

--- projects/company_payroll/hourlyemployee.py
+++ projects/company_payroll/hourlyemployee.py
@@ -1,5 +1,6 @@
 from employee import Employee
 
+
 class HourlyEmployee(Employee):
     """Class to create new Hourly Employee. It inherits from the base Employee class. HourlyEmployee(first:str, last:str, hours:float(default=40.0), rate:float(default=15))"""
 
@@ -11,12 +12,30 @@ class HourlyEmployee(Employee):
         super().__init__(first, last)
         self.hours_worked = hours
         self.hourly_rate = rate
-        self.ot_pay = 0
+        self.ot_hourly_rate = self.hourly_rate * 1.5
         self.ot_hours_worked = 0
+        self.regular_pay = 0
+        self.ytd_regular_pay = 0
+        self.ot_pay = 0
+        self.ytd_ot_pay = 0
 
     def __str__(self):
-        return f"""\nfirst: {self.first_name}\nlast: {self.last_name}\nhourly-rate: {self.hourly_rate:.2f}\nhours-worked: {self.hours_worked:.2f}\nregular-pay: ${self.current_gross_pay:.2f}
-ot-pay: ${self.ot_pay:.2f}\nbonus-pay: ${self.current_bonus_pay:.2f}\nytd-earnings: ${self.ytd_gross_pay:.2f}\nytd-bonus-pay: ${self.ytd_bonus_pay:.2f}"""
+        return f"""
+Employee(Hourly): Personal Info:
+first: {self.first_name}
+last: {self.last_name}
+\nRegular Pay Info:
+hourly-rate: {self.hourly_rate:.2f}
+hours-worked: {self.hours_worked:.2f}
+regular-pay: ${self.regular_pay:.2f}
+\nOvertime and Bonus Pay Info(if applicable):
+ot-pay: ${self.ot_pay:.2f}
+bonus-pay: ${self.current_bonus_pay:.2f}
+\nYear-To-Date:
+ytd-regular-pay: ${self.ytd_regular_pay:.2f}
+ytd-bonus-pay: ${self.ytd_bonus_pay:.2f}
+ytd-ot-pay: ${self.ytd_ot_pay:.2f}
+ytd-earnings: ${self.ytd_gross_pay:.2f}"""
 
     def __repr__(self):
         return f"""HourlyEmployee(first:str, last:str, hours:float(default=40.0), rate:float(default=15))"""
@@ -41,28 +60,38 @@ ot-pay: ${self.ot_pay:.2f}\nbonus-pay: ${self.current_bonus_pay:.2f}\nytd-earnin
         logging_message = f"Updated this week's hours worked to {hours_worked} for employee: {self.first_name}, {self.last_name}"
 
     def calculate_gross_pay(self) -> None:
-        if self.hours_worked == 0:
+        if self.hours_worked <= 0:
             print(
-                "Hours worked cannot be 0. Update work hours for this employee and try again."
+                "Hours worked cannot be less or equal to 0.\n \
+                Update work hours for this employee and try again."
             )
             return
         if self.hourly_rate < Employee.MINIMUM_WAGE:
             print(
-                f"The 'Hourly Rate' cannot be less than the legal minimum-wage: {Employee.MINIMUM_WAGE}."
+                f"The 'Hourly Rate' cannot be less than \
+                the legal minimum-wage: {Employee.MINIMUM_WAGE}."
             )
             print("Update hourly rate for thihs employee, and try again.")
             return
+
         self.ot_pay = 0
         self.ot_hours_worked = self.hours_worked - Employee.DEFAULT_WORK_HOURS
+
         if self.ot_hours_worked > 0:
-            self.ot_pay = self.ot_hours_worked * self.hourly_rate * 0.5
-        self.current_gross_pay = self.hourly_rate * self.hours_worked + self.ot_pay
+            self.ot_pay = self.ot_hours_worked * self.ot_hourly_rate
+            self.ytd_ot_pay += self.ot_pay
+
+        self.regular_pay = self.hourly_rate * Employee.DEFAULT_WORK_HOURS
+        self.ytd_regular_pay += self.regular_pay
+
+        self.current_gross_pay = self.regular_pay + self.ot_pay
         self.ytd_gross_pay += self.current_gross_pay
+
         Employee.SALARY_EXPENSE += self.current_gross_pay
 
     def calculate_bonus_pay(self, bonus_rate):
         multiplyer = bonus_rate / 100
-        self.current_bonus_pay = self.ytd_gross_pay * multiplyer
+        self.current_bonus_pay = self.ytd_regular_pay * multiplyer
         self.ytd_bonus_pay += self.current_bonus_pay
         self.ytd_gross_pay += self.current_bonus_pay
         Employee.SALARY_EXPENSE += self.current_bonus_pay
@@ -71,11 +100,10 @@ ot-pay: ${self.ot_pay:.2f}\nbonus-pay: ${self.current_bonus_pay:.2f}\nytd-earnin
 if __name__ == "__main__":
     hr_emp1 = HourlyEmployee("James", "Bond")
     hr_emp1.hours_worked = 50.00
-    hr_emp1.hourly_rate = 93.87
+    hr_emp1.hourly_rate = 46.45
     for i in range(52):
         # weekly salary based on 45 hours
         hr_emp1.calculate_gross_pay()
         if i == 51:
             hr_emp1.calculate_bonus_pay(15)
-
-    print(hr_emp1)
+        print(f"""\ncheck #\n{"100"+str(i) if i < 10 else "10"+str(i)}\n{hr_emp1}""")
