821c9f075985ebc8d94144a2e2676b21ac066d8f
@iamserda <12580399+iamserda@users.noreply.github.com>
2025-07-13 22:59:32 -0400

    new: company payroll system. Base employee class. -@iamserda

---
+++ projects/company_payroll/employee.py
@@ -0,0 +1,20 @@
+class Employee:
+    DEFAULT_WORK_HOURS = 40
+    MINIMUM_WAGE = 15
+    SALARY_EXPENSE = 0
+
+    def __init__(self, first, last):
+        self.first_name = first
+        self.last_name = last
+        self.ytd_gross_pay = 0
+        self.current_gross_pay = 0
+        self.ytd_bonus_pay = 0
+        self.current_bonus_pay = 0
+
+    def reset_year(self):
+        self.ytd_gross_pay = 0
+        self.current_gross_pay = 0
+        self.ytd_bonus_pay = 0
+        self.current_bonus_pay = 0
+
+
