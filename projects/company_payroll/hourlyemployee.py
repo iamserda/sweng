4ce976f05d6089d217e4728d0566f37093b0d4cf
@iamserda <12580399+iamserda@users.noreply.github.com>
2025-07-15 11:18:40 -0400

    restructured dunder str and dunder repr methods. repr is now fully compliant to python's standard which means when its output is used with eval(), it will reproduce the object, so it is essentially valid python code. Not always necessary but useful for automation and/or testing
    - update to dunder str emphasized legible code by formatting each line piece of info to be in its own row.
    
    - similar readability improvements were made to calculate_grosspay
    -@iamserda

--- projects/company_payroll/hourlyemployee.py
+++ projects/company_payroll/hourlyemployee.py
@@ -26,33 +26,41 @@ class HourlyEmployee(Employee):
         self.ytd_ot_pay: float = 0
 
     def __str__(self) -> str:
-        return f"""
-Employee(Hourly): Personal Info:
-first: {self.first_name}
-last: {self.last_name}
-\nRegular Pay Info:
-hourly-rate: {self.hourly_rate:.2f}
-hours-worked: {self.hours_worked:.2f}
-regular-pay: ${self.regular_pay:.2f}
-\nOvertime and Bonus Pay Info(if applicable):
-ot-pay: ${self.ot_pay:.2f}
-bonus-pay: ${self.current_bonus_pay:.2f}
-\nYear-To-Date:
-ytd-regular-pay: ${self.ytd_regular_pay:.2f}
-ytd-bonus-pay: ${self.ytd_bonus_pay:.2f}
-ytd-ot-pay: ${self.ytd_ot_pay:.2f}
-ytd-earnings: ${self.ytd_gross_pay:.2f}"""
+        return (
+            f"\nEmployee: Personal Info:"
+            f"\n- First: {self.first_name}"
+            f"\n- Last: {self.last_name}"
+            f"\n\nRegular Pay Info:"
+            f"\n- Hourly-Rate: {self.hourly_rate:.2f}"
+            f"\n- Hours Worked: {self.hours_worked:.2f}"
+            f"\n- Regular Pay: ${self.regular_pay:.2f}"
+            f"\n\nOvertime and Bonus Pay Info(if applicable):"
+            f"\n- OT Pay: ${self.ot_pay:.2f}"
+            f"\n- Bonus Pay: ${self.current_bonus_pay:.2f}"
+            f"\n\nYear-To-Date:"
+            f"\n- YTD-Regular-Pay: ${self.ytd_regular_pay:.2f}"
+            f"\n- YTD-Bonus-Pay: ${self.ytd_bonus_pay:.2f}"
+            f"\n- YTD-OT-Pay: ${self.ytd_ot_pay:.2f}"
+            f"\n- YTD-Earnings: ${self.ytd_gross_pay:.2f}\n"
+        )
 
     def __repr__(self) -> str:
-        return f"""HourlyEmployee(first:str, last:str, hours:float(default=40.0), rate:float(default=15))"""
+        return (
+            f"HourlyEmployee(first={repr(self.first_name)}, "
+            f"last={repr(self.last_name)}, "
+            f"hours={repr(self.hours_worked)}, "
+            f"rate={repr(self.hourly_rate)})"
+        )
 
     def update_hourly_rate(self, new_hourly_rate: float) -> None:
         if new_hourly_rate >= Employee.MINIMUM_WAGE:
             self.hourly_rate = new_hourly_rate
         else:
             print(
-                f"The 'Hourly Rate' must be equal or more than the minimum wage.\nMinimum wage is currently {Employee.MINIMUM_WAGE}."
+                "The 'Hourly Rate' must be equal or more than the minimum wage. "
+                f"\nMinimum wage is currently {Employee.MINIMUM_WAGE}."
             )
+        return
 
     def update_hours_worked(self, hours_worked: float) -> None:
         if hours_worked < 0:
@@ -68,14 +76,14 @@ ytd-earnings: ${self.ytd_gross_pay:.2f}"""
     def calculate_gross_pay(self) -> None:
         if self.hours_worked <= 0:
             print(
-                "Hours worked cannot be less or equal to 0.\n \
-                Update work hours for this employee and try again."
+                f"WARNING: Hours worked cannot be less or equal to 0. "
+                f"Update work hours for this employee and try again."
             )
             return
         if self.hourly_rate < Employee.MINIMUM_WAGE:
             print(
-                f"The 'Hourly Rate' cannot be less than \
-                the legal minimum-wage: {Employee.MINIMUM_WAGE}."
+                "The 'Hourly Rate' cannot be less than "
+                f"the legal minimum-wage: {Employee.MINIMUM_WAGE}."
             )
             print("Update hourly rate for thihs employee, and try again.")
             return
@@ -105,11 +113,11 @@ ytd-earnings: ${self.ytd_gross_pay:.2f}"""
 
 if __name__ == "__main__":
     hr_emp1 = HourlyEmployee("James", "Bond")
-    hr_emp1.hours_worked = 50.00
-    hr_emp1.hourly_rate = 46.45
+    hr_emp1.update_hourly_rate(10.00)
+    hr_emp1.update_hours_worked(50)
     for i in range(52):
         # weekly salary based on 45 hours
         hr_emp1.calculate_gross_pay()
         if i == 51:
             hr_emp1.calculate_bonus_pay(15)
-        print(f"""\ncheck #\n{"100"+str(i) if i < 10 else "10"+str(i)}\n{hr_emp1}""")
+        print(f"""\nCHECK #{"100"+str(i) if i < 10 else "10"+str(i)}\n{hr_emp1}""")
