11c7af153aa597b3e925ec7626980e908c555dba
@iamserda <12580399+iamserda@users.noreply.github.com>
2025-07-21 16:11:33 -0400

    using file name matching module in python - @iamserda

--- workingwithfiles/using_fnmatch.py
+++ workingwithfiles/using_fnmatch.py
@@ -4,6 +4,6 @@ import fnmatch
 def file_with_patterns(dir_path, pattern):
     for index, item in enumerate(os.listdir(dir_path)):
         if fnmatch.fnmatch(item, pat=pattern):
-            print(f"file no. {index + 1}: {item}")
+            print(f"{index + 1}: {item}")
 
-file_with_patterns("/volumes/ssd1tb/bookstore", "*python*.pdf")
+file_with_patterns(dir_path="/volumes/ssd1tb/bookstore", pattern="*_*.pdf")
