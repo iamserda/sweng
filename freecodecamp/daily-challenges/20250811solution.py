c413e6df1659e24a7127a4add5cb6c2e27921960
@iamserda <12580399+iamserda@users.noreply.github.com>
2025-11-03 19:19:13 -0500

    sol: 20250811 solved using two-pointer pattern in about 15 minutes because of focus/error. Lesson that I still need to learn, even after years of reps in, READ THE CODE before you hit submit! Run the mental Interpreter or Compiler! -@iamserda

---
+++ freecodecamp/daily-challenges/20250811solution.py
@@ -0,0 +1,50 @@
+"""
+Credit: FreeCodeCamp.org
+
+Original_Url: https://www.freecodecamp.org/learn/daily-coding-challenge/2025-08-11
+
+Date: 2025-08-11
+
+Challege:
+Given a string, determine whether the number of vowels in the first half of the string is equal to the number of vowels in the second half.
+The string can contain any characters.
+The letters a, e, i, o, and u, in either uppercase or lowercase, are considered vowels.
+If there's an odd number of characters in the string, ignore the center character.
+
+Test:
+1. is_balanced("racecar") should return True.
+2. is_balanced("Lorem Ipsum") should return True.
+3. is_balanced("Kitty Ipsum") should return False.
+4. is_balanced("string") should return False.
+5. is_balanced(" ") should return True.
+6. is_balanced("abcdefghijklmnopqrstuvwxyz") should return False.
+7. is_balanced("123A#b!E&*456-o.U") should return True.
+
+"""
+
+
+def is_balanced(s):
+    left = 0
+    right = len(s) - 1
+    vowel_set = set(list("aeiouAEIOU"))
+    left_count = 0
+    right_count = 0
+
+    while left <= right:
+        if left == right:
+            break
+        if s[left] in vowel_set:
+            left_count += 1
+        if s[right] in vowel_set:
+            right_count += 1
+        left += 1
+        right -= 1
+    return left_count == right_count
+
+assert is_balanced("racecar") is True
+assert is_balanced("Lorem Ipsum") is True
+assert is_balanced("Kitty Ipsum") is False
+assert is_balanced("string") is False
+assert is_balanced(" ") is True
+assert is_balanced("abcdefghijklmnopqrstuvwxyz") is False
+assert is_balanced("123A#b!E&*456-o.U") is True
